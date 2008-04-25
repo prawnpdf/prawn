d6ee92e36940595ac3407158a96a9c4e3b8f060a
sandal <sandal@seltzer.(none)>
2008-04-24 23:54:49 -0400

    Some specs for document

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,1 +1,57 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")  
+                  
+describe "When drawing a line" do
+     
+  class LineDrawingObserver
+    attr_accessor :points, :strokes
+
+    def initialize
+      @points = [] 
+      @strokes = 0
+    end
+
+    def append_line(*params)
+      @points << params
+    end    
+               
+    def begin_new_subpath(*params)
+      @points << params
+    end
+    
+    def stroke_path   
+      @strokes += 1
+    end             
+
+  end  
+  
+  before :each do
+     @pdf = Prawn::Document.new 
+  end
+ 
+  it "should draw and stroke a line from (100,600) to (100,500)" do
+    @pdf.line(100,600,100,500)
+    output = @pdf.render
+    
+    line_drawing = LineDrawingObserver.new
+    PDF::Reader.string(output, line_drawing)  
+    
+    line_drawing.points.should == [[100,600],[100,500]]       
+    line_drawing.strokes.should == 1
+  end  
+  
+  it "should draw and stroke two lines (100,600) to (100,500) " +
+     "and (75,100) to (50,125)" do 
+    @pdf.line(100,600,100,500) 
+    @pdf.line(75,100,50,125)
+    output = @pdf.render
+    
+    line_drawing = LineDrawingObserver.new
+    PDF::Reader.string(output,line_drawing) 
+    
+    line_drawing.points.should == 
+      [[100.0, 600.0], [100.0, 500.0], [75.0, 100.0], [50.0, 125.0]]
+    line_drawing.strokes.should == 2
+  end
+       
+end
+    
--- spec/pdf_object_spec.rb
+++ spec/pdf_object_spec.rb
@@ -46,12 +46,13 @@ describe "PDF Object Serialization" do
     dict = Prawn::PdfObject( :foo  => :bar, 
                              "baz"  => [1,2,3], 
                              :bang => {:a => "what", :b => [:you, :say] } )
-                            
-    dict[0..1].should == "<<"
-    dict[-2..-1].should == ">>"   
-    dict.should match(%r{/foo /bar\n})   
-    dict.should match(%r{/baz \[1 2 3\]\n})    
-    dict.should match(%r{/bang << /a \(what\)\n/b \[/you /say\]\n>>\n})
+                
+    res = parse_pdf_object(dict)           
+
+    res["foo"].should == "bar"
+    res["baz"].should == [1,2,3]
+    res["bang"].should == { "a" => "what", "b" => ["you", "say"] }
+
   end      
   
   it "should not allow keys other than strings or symbols for PDF dicts" do
