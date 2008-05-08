f3c60c00c4aca955fcea06379c2bbc378a599c25
Gregory Brown <user@eeepc-gregorybr.newhaven.local>
2008-05-08 10:57:12 -0400

    Hexagon

--- examples/hexagon.rb
+++ examples/hexagon.rb
@@ -3,7 +3,6 @@ require "prawn"
 
 pdf = Prawn::Document.new
                         
-pdf.stroke_color "000000"            
 pdf.fill_color "ff0000"
 pdf.fill_polygon [100, 250], [200, 300], [300, 250],
                  [300, 150], [200, 100], [100, 150]            
