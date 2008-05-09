14bcfafeb4aa05eb508ceb17bea02be731c7633c
sandal <sandal@seltzer.(none)>
2008-05-09 18:06:41 -0400

    Rudimentary support for built in fonts, needs refining.

--- examples/simple_text.rb
+++ examples/simple_text.rb
@@ -3,6 +3,7 @@ require "prawn"
 
 pdf = Prawn::Document.new 
 pdf.text "Hello World", :at => [200,720], :size => 32
-pdf.start_new_page
+pdf.start_new_page                       
+pdf.font "Courier"
 pdf.text "Goodbye World", :at => [288,50]
 pdf.render_file "hello.pdf"           
--- lib/prawn/document/text.rb
+++ lib/prawn/document/text.rb
@@ -6,23 +6,37 @@
 
 module Prawn
   class Document
-    module Text                       
+    module Text
+      
+      BUILT_INS = %w[ Courier Courier-Bold Courier-Oblique Courier-BoldOblique
+          Helvetica Helvetica-Bold Helvetica-Oblique Helvetica-BoldOblique
+          Times-Roman Times-Bold Times-Italic Times-BoldItalic
+          Symbol ZapfDingbats ]                      
       
       # temporary hack to get text rudiments working, will go away.       
-      def register_font
-        @font = ref(:Type     => :Font, 
-                    :Subtype  => :Type1, 
-                    :Name     => :F1,
-                    :BaseFont => :Helvetica,
-                    :Encoding => :MacRomanEncoding)  
+      def register_fonts
+        @fonts = {}   
+        @font = "Helvetica"
+        BUILT_INS.each_with_index do |f,i|       
+          @fonts[f] = ref(:Type     => :Font, 
+                          :Subtype  => :Type1, 
+                          :BaseFont => f.to_sym,
+                          :Encoding => :MacRomanEncoding)      
+        end
         @proc = ref [:PDF, :Text]
-      end       
+      end   
+      
+      def font(name)
+        @font = name
+        set_page_font
+      end   
                    
       # temporary hack to get text rudiments working, will go away.
-      def set_page_font
+      def set_page_font         
+        p @fonts
         @current_page.data[:Resources] =  { 
           :ProcSet => @proc, 
-          :Font    => { :F1 => @font }   
+          :Font    => { :F1 => @fonts[@font] }   
         }
       end
         
