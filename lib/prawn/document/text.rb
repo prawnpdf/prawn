0dcc85d05bd251bed85ae07700380b5a7ccc3f5d
sandal <sandal@seltzer.(none)>
2008-05-12 14:28:44 -0400

    Cleanup for text stuff, removing the pre-registering of built in fonts for nominally smaller files

--- lib/prawn/document/text.rb
+++ lib/prawn/document/text.rb
@@ -12,50 +12,56 @@ module Prawn
           Helvetica Helvetica-Bold Helvetica-Oblique Helvetica-BoldOblique
           Times-Roman Times-Bold Times-Italic Times-BoldItalic
           Symbol ZapfDingbats ]                      
-      
-      # temporary hack to get text rudiments working, will go away.       
-      def register_fonts
-        @fonts = {}   
-        @font = "Helvetica"
-        BUILT_INS.each_with_index do |f,i|       
-          @fonts[f] = ref(:Type     => :Font, 
-                          :Subtype  => :Type1, 
-                          :BaseFont => f.to_sym,
-                          :Encoding => :MacRomanEncoding)      
-        end
-        @proc = ref [:PDF, :Text]
-      end   
-      
-      def font(name)
-        @font = name
-        set_current_font
-      end   
-                   
-      def set_current_font #:nodoc:
-        @font_registry ||= {}
-        @font_registry[@fonts[@font]] ||= :"F#{@font_registry.size + 1}"            
-                
-        @current_page.data[:Resources] ||= { 
-          :ProcSet => @proc,             
-          :Font    => {}
-        }                                                      
-        
-       @current_page.data[:Resources][:Font].merge!(
-         @font_registry[@fonts[@font]] => @fonts[@font] 
-       ) 
-      end
-        
+              
       def text(text,options)        
         x,y = options[:at]
         add_content %Q{
         BT
-        /#{@font_registry[@fonts[@font]]} #{options[:size] || 12} Tf 
+        /#{font_registry[fonts[@font]]} #{options[:size] || 12} Tf 
         #{x} #{y} Td 
         #{Prawn::PdfObject(text)} Tj 
         ET           
         }
+      end 
+      
+      def register_font(name)   
+        raise unless BUILT_INS.include?(name)    
+        fonts[name] ||= ref(:Type     => :Font, 
+                         :Subtype  => :Type1, 
+                         :BaseFont => name.to_sym,
+                         :Encoding => :MacRomanEncoding)           
+      end   
+      
+      def font(name)
+        @font = name              
+        register_font(name)
+        set_current_font
+      end   
+                   
+      def set_current_font #:nodoc:                     
+        font "Helvetica" unless fonts[@font]
+        font_registry[fonts[@font]] ||= :"F#{font_registry.size + 1}"                                                                 
+          
+        @current_page.data[:Resources][:Font].merge!(
+          font_registry[fonts[@font]] => fonts[@font] 
+        )      
+      end     
+      
+      def font_registry #:nodoc:
+        @font_registry ||= {}
+      end     
+      
+      def font_proc #:nodoc:
+        @font_proc ||= ref [:PDF, :Text] 
+      end                    
+      
+      def fonts #:nodoc:
+        @fonts ||= {}
       end
       
+      private :set_current_font, :font_registry, :font_proc, :fonts
+        
     end
+    
   end
 end
