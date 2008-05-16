96d8399f235ba9b354c449d9e3a08010dc4a1f75
sandal <sandal@seltzer.(none)>
2008-05-16 17:56:46 -0400

    Miscellaneous work heading in the direction of flowing text.  Highlight is the Font::AFM module, ported by JEG2 and I from a CPAN module of the same name.  Other stuff not really working yet, sorry for the mess.

--- lib/prawn.rb
+++ lib/prawn.rb
@@ -3,8 +3,10 @@
 # Copyright April 2008, Gregory Brown.  All Rights Reserved.
 #
 # This is free software. Please see the LICENSE and COPYING files for details.
-
+           
+require "prawn/compatibility"
 require "prawn/errors"
 require "prawn/pdf_object"
 require "prawn/document"
-require "prawn/reference"
+require "prawn/reference"  
+require "prawn/font"
