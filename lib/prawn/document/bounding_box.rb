19d6b86441299e009d5cf917193fb950a245f707
Gregory Brown <user@eeepc-gregorybr.hsd1.ct.comcast.net>
2008-05-18 19:36:04 -0400

    Initial work on bounding boxen

---
+++ lib/prawn/document/bounding_box.rb
@@ -0,0 +1,47 @@
+module Prawn 
+  class Document
+    class BoundingBox
+
+      def initialize(point,options={})
+        @x,@y = point
+        @width, @height = options[:width], options[:height]
+      end
+
+      def anchor
+        [@x, @y - @height]
+      end
+
+      def left   
+        0 
+      end
+
+      def right
+        @width
+      end
+
+      def top
+        @height
+      end
+
+      def bottom
+        0
+      end
+
+      def absolute_left
+        @x
+      end
+
+      def absolute_right
+        @x + @width
+      end
+
+      def absolute_top
+        @y
+      end
+
+      def absolute_bottom
+        @y - @height
+      end
+    end
+  end
+end
