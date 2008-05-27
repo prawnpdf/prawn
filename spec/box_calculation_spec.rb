a6457d6bbc828a8a1be714430b1b4d647d12b0a8
Michael Daines <michael@mdaines.com>
2008-05-26 22:11:50 -0700

    test without the postmortem

--- spec/box_calculation_spec.rb
+++ spec/box_calculation_spec.rb
@@ -5,11 +5,11 @@ describe "A bounding box" do
   before(:each) { create_pdf }
   
   it "should calculate a height if none is specified" do
-    box = @pdf.bounding_box([100, 500], :width => 100) do
+    @pdf.bounding_box([100, 500], :width => 100) do
       @pdf.text "The rain in Spain falls mainly on the plains."
     end
     
-    box.height.should == 36
+    @pdf.y.should == 464
   end
 
 end
