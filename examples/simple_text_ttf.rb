fec3a9e6a9baae2676d0c9cd8162a3e3a535c8eb
Gregory Brown <gregory.t.brown@gmail.com>
2008-05-30 11:36:20 -0400

    TTF files require a matching afm file for metrics (for the time being).  Generate with ttf2afm or TTF2PT1

--- examples/simple_text_ttf.rb
+++ examples/simple_text_ttf.rb
@@ -3,8 +3,8 @@ require "prawn"
 
 Prawn::Document.generate "hello.pdf" do       
   fill_color "0000ff"
-  font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf", :ttf 
-  text "Hello World", :at => [200,720], :size => 32       
-  font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf", :ttf  
-  text "Blah Blah Blah"     
+  font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf" 
+  text "Hello World", :at => [200,720], :size => 32    
+  font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"
+  text "Blah Blah Blah" * 100
 end
