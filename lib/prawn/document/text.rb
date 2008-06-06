f2ab0c8f95d1e6ddb68270f06f529d0a5f32d24c
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-06 13:16:56 -0500

    80 chars per line pls. kthxbye

--- lib/prawn/document/text.rb
+++ lib/prawn/document/text.rb
@@ -29,25 +29,26 @@ module Prawn
       #
       # pdf.text "Hello World", :at => [100,100]
       # pdf.text "Goodbye World", :at => [50,50], :size => 16
-      # pdf.text "This will be wrapped when it hits the edge of your bounding box"
+      # pdf.text "Will be wrapped when it hits the edge of your bounding box"
       #
       # Under Ruby 1.8 compatible implementations, all strings passed to this
       # function should be encoded as UTF-8. If you gets unexpected characters
       # appearing in your rendered document, check this.
       #
-      # Under a M17n aware implementation (like Ruby 1.9), Prawn will attempt to
-      # convert the string to UTF-8 if necessary. An ArgumentError exception will
-      # be raised if this conversion fails.
+      # Under a M17n aware implementation (like Ruby 1.9), Prawn will attempt
+      # to convert the string to UTF-8 if necessary. An ArgumentError exception 
+      # will be raised if this conversion fails.
       #
-      # If an empty box is rendered to your PDF instead of the character you wanted
-      # it usually means the current font doesn't include that character.
+      # If an empty box is rendered to your PDF instead of the character you 
+      # wanted it usually means the current font doesn't include that character.
+      # 
       def text(text,options={})
-        # TODO: if the current font is a built in one, we can't use the utf-8 string
-        #       provided by the user. We should convert it to WinAnsi or MacRoman
-        #       or some such.
+        # TODO: if the current font is a built in one, we can't use the utf-8 
+        # string provided by the user. We should convert it to WinAnsi or 
+        # MacRoman or some such.
 
-        # if we're running under a M17n aware VM, ensure the string provided is UTF-8 or can be
-        # converted to UTF-8
+        # if we're running under a M17n aware VM, ensure the string provided is 
+        # UTF-8 or can be converted to UTF-8
         if text.respond_to?(:encode)
           begin
             text = text.encode("UTF-8")
@@ -88,15 +89,13 @@ module Prawn
       # array specifies the valid built in font values.
       #
       # pdf.font "Times-Roman"
-      # pdf.font "chalkboard.ttf"
+      # pdf.font "Chalkboard.ttf"
       #
-      # If a ttf font is specified, the full file will be embedded in the rendered
-      # PDF. This should be your preferred option in most cases. It will increase
-      # the size of the resulting file, but also make it more portable.
+      # If a ttf font is specified, the full file will be embedded in the 
+      # rendered PDF. This should be your preferred option in most cases. 
+      # It will increase the size of the resulting file, but also make it 
+      # more portable.
       #
-      # PERF: Cache or limit calls to this, no need to generate a
-      # new fontmetrics file or re-register the font each time.
-      
       def font(name)
         @font_metrics = Prawn::Font::Metrics[name]
         case(name)
@@ -183,7 +182,9 @@ module Prawn
 
         enctables[basename] = @font_metrics.enc_table
 
-        raise "#{file} missing the required encoding table" if enctables[basename].nil?
+        if enctables[basename].nil?
+          raise "#{file} missing the required encoding table" 
+        end
 
         font_content    = File.read(file)
         compressed_font = Zlib::Deflate.deflate(font_content)
@@ -208,9 +209,11 @@ module Prawn
                          )
 
         descendant = ref(:Type           => :Font,
-                         :Subtype        => :CIDFontType2, # CID, Type2 == CID, TTF
+                         :Subtype        => :CIDFontType2, # CID, TTF
                          :BaseFont       => basename,
-                         :CIDSystemInfo  => {:Registry => "Adobe", :Ordering => "Identity", :Supplement => 0},
+                         :CIDSystemInfo  => { :Registry   => "Adobe", 
+                                              :Ordering   => "Identity", 
+                                              :Supplement => 0 },
                          :FontDescriptor => descriptor,
                          :W              => @font_metrics.glyph_widths,
                          :CIDToGIDMap    => :Identity
