2b4bfaed619a6ea4d1543e1eb9177b56168831c5
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-06 00:10:52 -0500

    Lookup tables

--- examples/simple_text_ttf.rb
+++ examples/simple_text_ttf.rb
@@ -4,7 +4,7 @@ require "prawn"
 Prawn::Document.generate "hello-ttf.pdf" do       
   fill_color "0000ff"
   font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf" 
-  text "Hello World", :at => [200,720], :size => 32          
-#  font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"          
+  text "Hello World", :at => [200,720], :size => 32           
+  font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"          
   text "Blah Blah Blah " * 100           
 end
