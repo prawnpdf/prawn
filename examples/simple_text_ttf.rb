ec7975492ee98ce64f49f7f25bd81d6d72b3a09d
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-06 02:56:26 -0500

    No such luck.  Damn Adobe to hell

--- examples/simple_text_ttf.rb
+++ examples/simple_text_ttf.rb
@@ -3,8 +3,8 @@ require "prawn"
 
 Prawn::Document.generate "hello-ttf.pdf" do       
   fill_color "0000ff"
-  font "#{Prawn::BASEDIR}/data/fonts/Activa.ttf" 
+  font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf" 
   text "Hello World", :at => [200,720], :size => 32           
-  #font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"          
+  font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"          
   text "Blah Blah Blah " * 100           
 end
