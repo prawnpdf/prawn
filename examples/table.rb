b9e26a2f35917d6ee863d00f761bbe24fb0257d5
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-10 00:13:29 -0500

    Updated table

--- examples/table.rb
+++ examples/table.rb
@@ -2,5 +2,13 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
 
 Prawn::Document.generate("table.pdf") do 
-  table [["foo","baaar"],["This is","a sample"],["Table","dontchaknow?"]]
+  table [["foo",    "baaar",             "1" ],
+         ["This is","a sample",          "2" ],
+         ["Table",  "dont\ncha\nknow?",  "3" ],
+         [ "It",    "Rules",             "4" ]],     
+    :horizontal_spacing => 25,
+    :vertical_spacing   => 10,
+    :font_size          => 16 
+
+  text "This should appear just below the table at the original font size"
 end
--- lib/prawn/document/table.rb
+++ lib/prawn/document/table.rb
@@ -4,9 +4,12 @@ module Prawn
 
       attr_reader :col_widths
 
-      def initialize(data, document)
-        @data = data
-        @document = document
+      def initialize(data, document,options={})
+        @data               = data
+        @document           = document
+        @font_size          = options[:font_size] || 12
+        @horizontal_spacing = options[:horizontal_spacing] || 5
+        @vertical_spacing   = options[:vertical_spacing]  || 5
         calculate_column_widths
       end
       
@@ -14,26 +17,30 @@ module Prawn
         @col_widths = Hash.new(0)
         @data.each do |row|
           row.each_with_index do |cell,i|
-            length = @document.font_metrics.string_width(cell,12)
+            length = cell.lines.map { |e| 
+              @document.font_metrics.string_width(e,@font_size) }.max
             @col_widths[i] = length if length > @col_widths[i]
           end
         end
       end
 
       def draw
-        horizontal_spacing = 5
-        vertical_spacing   = 5
-        y = @document.y
-        @data.each do |row|
-          x = @document.bounds.absolute_left
-          row.each_with_index do |col,i|
-            width = @col_widths[i]
-            @document.bounding_box([x,y], :width => width) do
-              @document.text(col)
+        @document.font_size(@font_size) do
+          y = @document.y
+          @data.each do |row|
+            x = @document.bounds.absolute_left
+            lines = 1
+            row.each_with_index do |col,i|
+              col_lines = col.lines.length
+              lines = col_lines if col_lines > lines
+              width = @col_widths[i]
+              @document.bounding_box([x,y], :width => width) do
+                @document.text(col)
+              end
+              x += width + @horizontal_spacing
             end
-            x += width + horizontal_spacing
+            y -= (@document.font_metrics.font_height(@font_size) * lines) + @vertical_spacing
           end
-          y -= @document.font_metrics.font_height(12) + vertical_spacing
         end
       end
 
