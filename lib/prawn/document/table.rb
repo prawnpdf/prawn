6d100da857e47abfae66366d2a84c1a1e901f1c0
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-11 14:57:46 -0500

    Painful POC of tables. I'm going to implement text boxes and then revisit

--- lib/prawn/document/table.rb
+++ lib/prawn/document/table.rb
@@ -14,7 +14,7 @@ module Prawn
       end
       
       def calculate_column_widths
-        @col_widths = Hash.new(0)
+        @col_widths = [0] * @data[0].length
         @data.each do |row|
           row.each_with_index do |cell,i|
             length = cell.lines.map { |e| 
@@ -39,7 +39,31 @@ module Prawn
               end
               x += width + @horizontal_spacing
             end
-            y -= (@document.font_metrics.font_height(@font_size) * lines) + @vertical_spacing
+
+            row_height = @document.font_metrics.font_height(@font_size) * lines
+
+            y -= (row_height) + @vertical_spacing
+
+            # TODO: Need a shortcut for an absolute bounding box.
+
+            @document.stroke_line [0, y - @document.bounds.absolute_bottom], 
+                                  [x - @document.bounds.absolute_left, y - @document.bounds.absolute_bottom]
+
+            @document.stroke_line [0, y + row_height - @document.bounds.absolute_bottom],
+                                  [x - @document.bounds.absolute_left, y + row_height - @document.bounds.absolute_bottom]
+
+            x = @document.bounds.left
+
+            @document.stroke_line [x, y + row_height - @document.bounds.absolute_bottom],
+                                  [x, y - @document.bounds.absolute_bottom]
+
+            @col_widths.each do |w|
+              x += w + @horizontal_spacing / 2.0
+             @document.stroke_line [x, y + row_height - @document.bounds.absolute_bottom],
+                                   [x, y - @document.bounds.absolute_bottom]
+              x += @horizontal_spacing / 2.0
+            end
+
           end
         end
       end
