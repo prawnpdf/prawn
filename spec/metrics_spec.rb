c672c8df9a3005e4e8cc574b09a598ba925ad2a4
Michael Daines <michael@mdaines.com>
2008-06-22 11:20:03 -0700

    failing latin tests

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -1,18 +1,25 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")   
 
-describe "font metrics" do
+describe "adobe font metrics" do
+  
+  setup do
+    @times = Prawn::Font::Metrics["Times-Roman"]
+  end
+  
+  it "should calculate string width taking into account accented characters" do
+    @times.string_width("é", 12).should == @times.string_width("e", 12)
+  end
   
   it "should calculate string width taking into account kerning pairs" do
-    times = Prawn::Font::Metrics["Times-Roman"]
-    times.string_width("To", 12).should == 13.332
-    times.string_width("To", 12, :kerning => true).should == 12.372
+    @times.string_width("To", 12).should == 13.332
+    @times.string_width("To", 12, :kerning => true).should == 12.372
+    @times.string_width("Tö", 12, :kerning => true).should == 12.372
   end
   
   it "should kern a string" do
-    times = Prawn::Font::Metrics["Times-Roman"]
-    
-    times.kern("To").should == ["T", -80, "o"]
-    times.kern("Technology").should == ["T", -70, "echnology"]
+    @times.kern("To").should == ["T", -80, "o"]
+    @times.kern("Tö").should == ["T", -80, "ö"]
+    @times.kern("Technology").should == ["T", -70, "echnology"]
   end
   
 end
