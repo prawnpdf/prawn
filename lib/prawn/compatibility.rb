97a917976af44437c0c7e721ba56040a179218eb
Michael Daines <michael@mdaines.com>
2008-06-22 11:07:20 -0700

    add a kern method to metrics, use that in string_width

--- lib/prawn/compatibility.rb
+++ lib/prawn/compatibility.rb
@@ -1,5 +1,12 @@
-if RUBY_VERSION < "1.9"  
+if RUBY_VERSION < "1.9"
+  require "strscan"
+  
   class String
     alias_method :lines, :to_a
+    
+    def each_char
+      scanner, char = StringScanner.new(self), /./mu
+      loop { yield(scanner.scan(char) || break) }
+    end
   end
 end 
--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -2,17 +2,17 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")
 
 describe "font metrics" do
   
-  it "should make the kerning pairs available" do
-    times = Prawn::Font::Metrics["Times-Roman"]
-    times.kerning("T", "o").should == -80
-    times.kerning("X", "X").should == 0
-    times.kerning(nil, "X").should == 0
-  end
-  
   it "should calculate string width taking into account kerning pairs" do
     times = Prawn::Font::Metrics["Times-Roman"]
     times.string_width("To", 12).should == 13.332
     times.string_width("To", 12, :kerning => true).should == 12.372
+  end
+  
+  it "should kern a string" do
+    times = Prawn::Font::Metrics["Times-Roman"]
+    
+    times.kern("To").should == ["T", -80, "o"]
+    times.kern("Technology").should == ["T", -70, "echnology"]
   end
   
 end
