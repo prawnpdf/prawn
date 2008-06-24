e40a86a6c3ce0414f6e3fd4abfffbc71d00bb20e
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-24 12:48:17 -0500

    And the world rejoices as bugs are fixed in TTF font_height

--- examples/table.rb
+++ examples/table.rb
@@ -2,7 +2,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
 
 Prawn::Document.generate("table.pdf") do 
-  font "#{Prawn::BASEDIR}/data/fonts/DejaVuSans.ttf"
+  font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf"
   table [["foo",    "baaar",             "1" ],
          ["This is","a sample",          "2" ],
          ["Table",  "dont\ncha\nknow?",  "3" ],
@@ -27,8 +27,8 @@ Prawn::Document.generate("table.pdf") do
          [ "It",    "Rules",             "4" ]],     
 
     :font_size  => 18, 
-    :horizontal_padding => 3,
-    :vertical_padding => 5,
+    :horizontal_padding => 10,
+    :vertical_padding => 3,
     :border     => 2,
     :position   => :center,
     :headers    => ["Column A","Column B","#"]
