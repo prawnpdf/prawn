eec1dc3021fac4d875e11bbafb9834c401c8851c
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-26 18:31:06 -0500

    Clean up example

--- examples/simple_text_ttf.rb
+++ examples/simple_text_ttf.rb
@@ -5,6 +5,10 @@ Prawn::Document.generate "hello-ttf.pdf" do
   fill_color "0000ff"
   font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf" 
   text "Hello World", :at => [200,720], :size => 32           
+
   font "#{Prawn::BASEDIR}/data/fonts/Chalkboard.ttf"
-  text "This is chalkboard wrapping " * 20
+
+  pad(20) do
+    text "This is chalkboard wrapping " * 20
+  end
 end
