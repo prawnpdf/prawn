f6cc413921cf7fbea56d69b3b1d1d7a009562377
Gregory Brown <gregory.t.brown@gmail.com>
2008-06-26 18:08:12 -0500

    Cell API docs [#26]

--- lib/prawn/graphics/cell.rb
+++ lib/prawn/graphics/cell.rb
@@ -1,6 +1,44 @@
 module Prawn
+
+  class Document
+    # Builds and renders a Graphics::Cell.  A cell is essentially a
+    # special-purpose bounding box designed for flowing text within a bordered
+    # area.  For available options, see Graphics::Cell#new.
+    #
+    #    Prawn::Document.generate("cell.pdf") do
+    #       cell [100,500], 
+    #         :width => 200,
+    #         :text  => "The rain in Spain falls mainly on the plains"
+    #    end
+    #
+    def cell(point, options={})
+      Prawn::Graphics::Cell.new(options.merge(:document => self, :point => point)).draw
+    end
+  end
+
   module Graphics
+    # A cell is a special-purpose bounding box designed to flow text within a
+    # bordered area. This is used by Prawn's Document::Table implementation but
+    # can also be used standalone for drawing text boxes via Document#cell
+    #
     class Cell
+
+      # Creates a new cell object.  Generally used indirectly via Document#cell
+      #
+      # Of the available options listed below, <tt>:point</tt>, <tt>:width</tt>,
+      # and <tt>:text</tt> must be provided. If you are not using the
+      # Document#cell shortcut, the <tt>:document</tt> must also be provided.
+      #
+      # <tt>:point</tt>:: Absolute [x,y] coordinate of the top-left corner of the cell.
+      # <tt>:document</tt>:: The Prawn::Document object to render on. 
+      # <tt>:text</tt>:: The text to be flowed within the cell
+      # <tt>:width</tt>:: The width in PDF points of the cell.
+      # <tt>:border</tt>:: The border line width. If omitted, no border will be drawn.
+      # <tt>:horizontal_padding</tt>:: The horizontal padding in PDF points
+      # <tt>:vertical_padding</tt>:: The vertical padding in PDF points
+      # <tt>:padding</tt>:: Overrides both horizontal and vertical padding
+      # <tt>:border_style</tt>:: One of <tt>:all</tt>, <tt>:no_top<tt>, <tt>:no_bottom</tt>, <tt>:sides</tt>
+      #
       def initialize(options={})
         @point        = options[:point]
         @document     = options[:document]
@@ -18,27 +56,37 @@ module Prawn
       end
 
       attr_accessor :point, :border_style, :border
-      attr_writer   :height
+      attr_writer   :height #:nodoc:
 
+      # The width of the text area excluding the horizonal padding
+      #
       def text_area_width
         width - 2*@horizontal_padding
       end
 
+      # The width of the cell in PDF points
+      #
       def width
         @width || (@document.font_metrics.string_width(@text,
           @document.current_font_size)) + 2*@horizontal_padding
       end
 
+      # The height of the cell in PDF points
+      #
       def height
         @height || text_area_height + 2*@vertical_padding
       end
 
+      # The height of the text area excluding the vertical padding
+      #
       def text_area_height
         @document.font_metrics.string_height(@text, 
          :font_size  => @document.current_font_size, 
          :line_width => text_area_width) 
       end
 
+      # Draws the cell onto the PDF document
+      # 
       def draw
         rel_point = [@point[0] - @document.bounds.absolute_left,
                      @point[1] - @document.bounds.absolute_bottom]
@@ -82,6 +130,8 @@ module Prawn
         end
       end
 
+      private
+
       def borders
         @borders ||= case @border_style
         when :all
@@ -97,8 +147,10 @@ module Prawn
 
     end
 
-    # TODO: A temporary, entertaining name that should probably be changed.
-    class CellBlock
+    class CellBlock #:nodoc:
+
+      # Not sure if this class is something I want to expose in the public API.
+
       def initialize(document)
         @document = document
         @cells    = []
@@ -157,9 +209,4 @@ module Prawn
     end
   end
  
-  class Document
-    def cell(point, options={})
-      Prawn::Graphics::Cell.new(options.merge(:document => self, :point => point)).draw
-    end
-  end
 end
