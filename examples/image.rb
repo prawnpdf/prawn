9dbda1baefbe902488891a527761a9a4258e7193
Gregory Brown <gregory.t.brown@gmail.com>
2008-07-24 19:32:07 -0400

    Fix some bugs and make the example much more awesomer

--- examples/image.rb
+++ examples/image.rb
@@ -3,7 +3,12 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
    
-Prawn::Document.generate("image.pdf") do 
-  filename = File.join("#{Prawn::BASEDIR}/data/images/stef.jpg")
-  image filename, :at => [200, 400]
+Prawn::Document.generate("image.pdf", :page_layout => :landscape) do 
+  pigs = "#{Prawn::BASEDIR}/data/images/pigs.jpg" 
+  image pigs, :at => [50,550]                                        
+  
+  stef = "#{Prawn::BASEDIR}/data/images/stef.jpg"
+  image stef, :at => [75, 75]
+  
+  text "Please enjoy the pigs", :size => 36, :at => [200,15]   
 end
