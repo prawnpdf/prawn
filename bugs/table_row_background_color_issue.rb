2385e557abf945f3ba5c8f0ad61be861a9293cc4
Gregory Brown <gregory.t.brown@gmail.com>
2008-07-24 12:55:06 -0400

    Let's track down this bug in table row coloring

--- bugs/table_row_background_color_issue.rb
+++ bugs/table_row_background_color_issue.rb
@@ -1,9 +1,11 @@
-# encoding: utf-8
+# As of 96f660660345c7c22923ba51d0124022a3a189ab, table is currently not taking
+# in account border widths when filling in rows with background coloring.  This
+# means the larger the border, the larger the visible gap between rows.
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
 
-Prawn::Document.generate("table.pdf") do 
+Prawn::Document.generate("table_with_background_color_problems.pdf") do 
   font "#{Prawn::BASEDIR}/data/fonts/DejaVuSans.ttf"
   table [["ὕαλον ϕαγεῖν",    "baaar",             "1" ],
          ["This is","a sample",          "2" ],
@@ -33,7 +35,9 @@ Prawn::Document.generate("table.pdf") do
     :vertical_padding => 3,
     :border     => 2,
     :position   => :center,
-    :headers    => ["Column A","Column B","#"]
+    :headers    => ["Column A","Column B","#"],
+    :row_colors => ["eeeeee"]
+
   pad(20) do
     text "This should appear in the original font size"
   end
