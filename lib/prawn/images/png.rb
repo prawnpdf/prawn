1eca3d1e45b04c3a949be568ea1056c5cb0a6f16
James Healy <jimmy@deefa.com>
2008-07-26 16:19:00 +1000

    small cleanup of the PNG class - simplify the args needed by the constructor - add a few comments and encoding declaration - remove some magic numbers

--- lib/prawn/images/png.rb
+++ lib/prawn/images/png.rb
@@ -1,17 +1,31 @@
+# encoding: utf-8
+
+# png.rb : Extracts the data from a PNG that is needed for embedding
+#
+# Based on some similar code in PDF::Writer by Austin Ziegler
+#
+# Copyright April 2008, James Healy.  All Rights Reserved.
+#
+# This is free software. Please see the LICENSE and COPYING files for details.
+
 require 'stringio'
 
 module Prawn
   module Images
-    class PNG
-      attr_accessor :palette, :img_data, :transparency
+    # A convenience class that wraps the logic for extracting the parts
+    # of a PNG image that we need to embed them in a PDF
+    class PNG #nodoc
+      attr_reader :palette, :img_data, :transparency
 
-      def initialize(data, color_type)
+      # Process a new PNG image
+      #
+      # <tt>:data</tt>:: A string containing a full PNG file
+      def initialize(data)
         data = StringIO.new(data.dup)
 
         data.read(8)  # Skip the default header
 
         ok        = true
-        length    = data.size
         @palette  = ""
         @img_data = ""
 
@@ -19,36 +33,49 @@ module Prawn
           chunk_size  = data.read(4).unpack("N")[0]
           section     = data.read(4)
           case section
+          when 'IHDR'
+            # we can grab other interesting values from here (like width,
+            # height, etc)
+            case data.read(chunk_size).unpack("NNCCCCC")[3]
+            when 0
+              @color_type = :greyscale
+            when 2
+              @color_type = :rgb
+            when 3
+              @color_type = :indexed
+            end
           when 'PLTE'
-            palette << data.read(chunk_size)
+            @palette << data.read(chunk_size)
           when 'IDAT'
             @img_data << data.read(chunk_size)
           when 'tRNS'
             # This chunk can only occur once and it must occur after the
             # PLTE chunk and before the IDAT chunk
             @transparency = {}
-            case color_type
-            when 3
+            case @color_type
+            when :indexed
               # Indexed colour, RGB. Each byte in this chunk is an alpha for
               # the palette index in the PLTE ("palette") chunk up until the
               # last non-opaque entry. Set up an array, stretching over all
               # palette entries which will be 0 (opaque) or 1 (transparent).
               @transparency[:type]  = 'indexed'
               @transparency[:data]  = data.read(chunk_size).unpack("C*")
-            when 0
+            when :greyscale
               # Greyscale. Corresponding to entries in the PLTE chunk.
               # Grey is two bytes, range 0 .. (2 ^ bit-depth) - 1
               @transparency[:grayscale] = data.read(2).unpack("n")
               @transparency[:type]      = 'indexed'
-            when 2
+            when :rgb
               # True colour with proper alpha channel.
               @transparency[:rgb] = data.read(6).unpack("nnn")
             end
+          when 'IEND'
+            # we've got everything we need, exit the loop
+            ok = false
           else
+            # unknown (or un-important) section, skip over it
             data.seek(data.pos + chunk_size)
           end
-
-          ok = (section != "IEND")
 
           data.read(4)  # Skip the CRC
         end
