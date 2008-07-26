cc90cbf3bfe4df874927113fb36a0b81c3de58b6
Gregory Brown <gregory.t.brown@gmail.com>
2008-07-26 14:21:15 -0400

    Rework example to show off all the shiny stuff

--- examples/image.rb
+++ examples/image.rb
@@ -3,19 +3,15 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
    
-Prawn::Document.generate("image.pdf") do     
-=begin
+Prawn::Document.generate("image.pdf", :page_layout => :landscape) do     
   pigs = "#{Prawn::BASEDIR}/data/images/pigs.jpg" 
-  image pigs, :at => [50,550]                                        
+  image pigs, :at => [50,450], :width => 302, :height => 227                                       
   
   stef = "#{Prawn::BASEDIR}/data/images/stef.jpg"
   image stef, :at => [75, 75]
   
   text "Please enjoy the pigs", :size => 36, :at => [200,15]   
-=end
   
-#=begin
-   ruport = "#{Prawn::BASEDIR}/data/images/google.png"  
-   canvas { image ruport, :at => [100,605], :width => 300, :height => 300 }
-#=end
+  ruport = "#{Prawn::BASEDIR}/data/images/ruport.png"  
+  image ruport, :at => [400,400] 
 end
