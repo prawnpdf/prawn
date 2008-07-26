eb48d94aaae93f335e0abe9dfad19583de5a0867
James Healy <jimmy@deefa.com>
2008-07-26 15:56:25 +1000

    Use StringIO instead of ImageInfo::OffsetReader in the PNG class

--- lib/prawn/images/png.rb
+++ lib/prawn/images/png.rb
@@ -1,13 +1,14 @@
+require 'stringio'
+
 module Prawn
   module Images
     class PNG
       attr_accessor :palette, :img_data, :transparency
 
       def initialize(data, color_type)
-        data = data.dup
-        data.extend(ImageInfo::OffsetReader)
+        data = StringIO.new(data.dup)
 
-        data.read_o(8)  # Skip the default header
+        data.read(8)  # Skip the default header
 
         ok        = true
         length    = data.size
@@ -15,13 +16,13 @@ module Prawn
         @img_data = ""
 
         while ok
-          chunk_size  = data.read_o(4).unpack("N")[0]
-          section     = data.read_o(4)
+          chunk_size  = data.read(4).unpack("N")[0]
+          section     = data.read(4)
           case section
           when 'PLTE'
-            palette << data.read_o(chunk_size)
+            palette << data.read(chunk_size)
           when 'IDAT'
-            @img_data << data.read_o(chunk_size)
+            @img_data << data.read(chunk_size)
           when 'tRNS'
             # This chunk can only occur once and it must occur after the
             # PLTE chunk and before the IDAT chunk
@@ -33,23 +34,23 @@ module Prawn
               # last non-opaque entry. Set up an array, stretching over all
               # palette entries which will be 0 (opaque) or 1 (transparent).
               @transparency[:type]  = 'indexed'
-              @transparency[:data]  = data.read_o(chunk_size).unpack("C*")
+              @transparency[:data]  = data.read(chunk_size).unpack("C*")
             when 0
               # Greyscale. Corresponding to entries in the PLTE chunk.
               # Grey is two bytes, range 0 .. (2 ^ bit-depth) - 1
-              @transparency[:grayscale] = data.read_o(2).unpack("n")
+              @transparency[:grayscale] = data.read(2).unpack("n")
               @transparency[:type]      = 'indexed'
             when 2
               # True colour with proper alpha channel.
-              @transparency[:rgb] = data.read_o(6).unpack("nnn")
+              @transparency[:rgb] = data.read(6).unpack("nnn")
             end
           else
-            data.offset += chunk_size
+            data.seek(data.pos + chunk_size)
           end
 
           ok = (section != "IEND")
 
-          data.read_o(4)  # Skip the CRC
+          data.read(4)  # Skip the CRC
         end
       end
     end
