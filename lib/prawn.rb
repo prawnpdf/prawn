1c1cf28eead6b7475244df8b6ed16d88d75ac6af
Gregory Brown <gregory.t.brown@gmail.com>
2008-08-11 13:20:41 -0400

    Version numbering

--- lib/prawn.rb
+++ lib/prawn.rb
@@ -23,11 +23,12 @@ end
 
 require 'ttf'
 
-module Prawn 
+module Prawn   
   file = __FILE__
   file = File.readlink(file) if File.symlink?(file)
   dir = File.dirname(file)
                           
   # The base source directory for Prawn as installed on the system
-  BASEDIR = File.expand_path(File.join(dir, '..'))    
+  BASEDIR = File.expand_path(File.join(dir, '..'))
+  VERSION = 0.1.2    
 end
