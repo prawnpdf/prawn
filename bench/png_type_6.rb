dc01d774d3abb7fde07181db44c927659b09f07f
Gregory Brown <gregory.t.brown@gmail.com>
2008-08-18 23:39:48 -0400

    PNG Type 6 bench

---
+++ bench/png_type_6.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')   
+require "prawn"        
+require "benchmark"    
+
+N=5
+     
+Benchmark.bmbm do |x|         
+  x.report("PNG Type 6") do     
+    N.times do
+      Prawn::Document.new do 
+        image "#{Prawn::BASEDIR}/data/images/dice.png"
+      end.render
+    end         
+  end   
+end
