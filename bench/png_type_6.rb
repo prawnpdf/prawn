c1736bc413ebe9a233de0ebecaff9bf967ae8765
Gregory Brown <gregory.t.brown@gmail.com>
2008-08-19 02:14:10 -0400

    Enough for now, will read over the spec tomorrow

--- bench/png_type_6.rb
+++ bench/png_type_6.rb
@@ -2,18 +2,14 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"        
 require "benchmark"   
 
-#=begin
 N=5
      
 Benchmark.bmbm do |x|         
   x.report("PNG Type 6") do     
     N.times do
-#=end
       Prawn::Document.new do 
         image "#{Prawn::BASEDIR}/data/images/dice.png"
       end.render
-#=begin
     end         
   end   
 end   
-#=end
