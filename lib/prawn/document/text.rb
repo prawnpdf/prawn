prawnpdf/prawn

commit c92a03685cd013af8767e7327b3a5def74a0a2ff

Minimal changes to support character based wrapping and removal of offset x position artifacts [#67]

lib/prawn/document/text.rb
@@ -88,11 +88,10 @@ module Prawn
       # TODO: Get kerning working with wrapped text
       def wrapped_text(text,options) 
         options[:align] ||= :left      
-        @text_x_pos ||= 0 
-        
+
         font.size(options[:size]) do
           text = font.metrics.naive_wrap(text, bounds.right, font.size, 
-            :kerning => options[:kerning], :offset => @text_x_pos ) 
+            :kerning => options[:kerning], :mode => options[:wrap]) 
 
           lines = text.lines
                                                        
@@ -103,18 +102,17 @@ module Prawn
             line_width = font.width_of(e)
             case(options[:align]) 
             when :left
-              x = @bounding_box.absolute_left + @text_x_pos
+              x = @bounding_box.absolute_left
             when :center
-              x = @bounding_box.absolute_left + @text_x_pos +
+              x = @bounding_box.absolute_left
                 (@bounding_box.width - line_width) / 2.0
             when :right
-              x = @bounding_box.absolute_right - line_width - @text_x_pos
+              x = @bounding_box.absolute_right - line_width 
             end
                                
             add_text_content(e,x,y,options)
             ds = -font.metrics.descender / 1000.0 * font.size 
             move_text_position(options[:spacing] || ds )     
-            @text_x_pos = 0
           end 
         end
       end  

examples/chinese_text_wrapping.rb
@@ -1,0 +1,11 @@
+# coding: utf-8
+
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+require "prawn"
+
+start = Time.now
+Prawn::Document.generate("chinese_flow.pdf") do  
+  font "#{Prawn::BASEDIR}/data/fonts/gkai00mp.ttf"
+  long_text = "更可怕的是，同质化竞争对手可以按照URL中后面这个ID来遍历您的DB中的内容，写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事，这样的话，你就非常被动了。写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事写个小爬虫把你的页面上的关键信息顺次爬下来也不是什么难事"                                                       
+  text long_text, :wrap => :character 
+end

lib/prawn/font/wrapping.rb
@@ -7,14 +7,17 @@
 # This is free software. Please see the LICENSE and COPYING files for details.
 module Prawn
   class Font #:nodoc:
-    module Wrapping #:nodoc:
+    module Wrapping #:nodoc:                
+      $KCODE="U"
       
       # TODO: Replace with TeX optimal algorithm
       def naive_wrap(string, line_width, font_size, options = {})
+        accumulated_width = 0
+        scan_pattern = options[:mode] == :character ? /./ : /\S+|\s+/                                    
+        
         output = ""                
-        accumulated_width = options[:offset] || 0 
-        string.lines.each do |line|
-          segments = line.scan(/\S+|\s+/)
+        string.lines.each do |line|         
+          segments = line.scan(scan_pattern)
                                         
           segments.each do |segment|    
             segment_width = string_width(segment, font_size, 
@@ -34,8 +37,6 @@ module Prawn
               accumulated_width += segment_width
             end
           end    
-          
-          accumulated_width = 0
         end
   
         output

commit adc37a4760b78c535cd1e6ebe48eea21d0e3945d

Fixing a bug on text centering introduced in c92a03685cd013af8767e7327b3a5def74a0a2ff.

lib/prawn/document/text.rb
@@ -106,7 +106,7 @@ module Prawn
             when :left
               x = @bounding_box.absolute_left
             when :center
-              x = @bounding_box.absolute_left
+              x = @bounding_box.absolute_left +
                 (@bounding_box.width - line_width) / 2.0
             when :right
               x = @bounding_box.absolute_right - line_width 