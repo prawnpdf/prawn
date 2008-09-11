9a0d57a2578c48d6e82b253fab256a38e94e4e81
Peter K Ko <peterkko@pkk.boulder.foraker.com>
2008-09-11 11:14:57 -0600

    Table header color example added

---
+++ examples/table_header_color.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+require "prawn"
+
+Prawn::Document.generate "table_header_color.pdf" do
+  table [ ['01/01/2008', 'John Doe', '4.2', '125.00', '525.00'], ['01/12/2008', 'Jane Doe', '3.2', '75.50', '241.60'] ],
+  :position => :center,
+  :headers => ['Date', 'Employee', 'Hours', 'Rate', 'Total'],
+  :widths => { 0 => 75, 1 => 100, 2 => 50, 3 => 50, 4 => 50},
+  :border_style => :grid,
+  :header_color => 'EFEFEF'
+end
