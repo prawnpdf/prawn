f15ce1d1549b0dab3987a55c65878baad241a43a
esb <eastsydneyboy@gmail.com>
2008-10-14 00:04:29 -0700

    Add :skip_encoding and :text_options to verify_options list

--- lib/prawn/document.rb
+++ lib/prawn/document.rb
@@ -78,7 +78,7 @@ module Prawn
     def initialize(options={},&block)   
        Prawn.verify_options [:page_size, :page_layout, :on_page_start,
          :on_page_stop, :left_margin, :right_margin, :top_margin,
-         :bottom_margin, :skip_page_creation, :compress ], options
+         :bottom_margin, :skip_page_creation, :compress, :skip_encoding, :text_options ], options
          
        @objects = []
        @info    = ref(:Creator => "Prawn", :Producer => "Prawn")
