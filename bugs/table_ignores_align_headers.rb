786146a2583d3e51f7b6dc804b94e93b9bbef2d0
Gregory Brown <gregory.t.brown@gmail.com>
2008-11-14 09:27:26 -0500

    Investigating :align_headers issue [#119]
    It appears as if when :border_style => :grid, header alignment is ignored.
    With a default border style, the problem cannot be reproduced.  I have updated
    the example slightly and will investigate further

--- bugs/table_ignores_align_headers.rb
+++ bugs/table_ignores_align_headers.rb
@@ -2,11 +2,21 @@
 # Prawn ignores :align_headers property in tables
 # when :border_style => :grid is present (Lighthouse issue #119).
 #
+# NOTES: 
+# 
+#  * This issue can only be reproduced when :border_style => :grid is used
+#
+$DEBUG = true
+
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require "prawn"
 
 Prawn::Document.generate("table_ignores_align_headers.pdf") do
-  left = "Left\njustified"
-  center = "Should\nbe\ncentered"
-  table [[left, left], [left, left]], :headers => [center, center], :align => :left, :align_headers => :center, :border_style => :grid
+  left  = "Left justified"
+  left2 = "left"
+  center = "centered"
+  table [[left, left], [left2, left2]], :headers       => [center, center], 
+                                      :align         => :left, 
+                                      :align_headers => :center,
+                                      :border_style  => :grid
 end
