f640daf2ee9188a9297f4f7c4e36ca3b96343e1c
Brad Ediger <brad.ediger@madriska.com>
2009-08-31 22:57:21 -0500

    Added README and one example

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -1,3 +1,4 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'prawn/core'
+require 'prawn/security'
 Prawn.debug = true
---
+++ examples/hello_foo.rb
@@ -0,0 +1,8 @@
+require File.join(File.dirname(__FILE__), "example_helper")
+
+Prawn::Document.generate("hello_foo.pdf") do
+  text "Hello, world!"
+  encrypt_document :user_password => 'foo', :owner_password => 'bar',
+    :permissions => { :print_document => false }
+end
+
