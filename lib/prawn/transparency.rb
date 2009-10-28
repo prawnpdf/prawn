c7c67444c80c07b41abfaede63cbdd041b068c3e
Daniel Nelson <daniel@Optiplex760.(none)>
2009-10-27 23:17:33 -0500

    Passing specs

--- lib/prawn/transparency.rb
+++ lib/prawn/transparency.rb
@@ -11,6 +11,9 @@ module Prawn
   module Transparency
     # , &block
     #  raise "block required" unless block_given?
+    def restore_opacity
+      add_content "Q"
+    end
     def set_opacity(opacity, stroke_opacity=opacity)
       min_version(1.4)
       key = "#{opacity}_#{stroke_opacity}"
@@ -19,21 +22,18 @@ module Prawn
         opacity_dictionary_name =  opacity_dictionary_registry[key][:name]
       else
         opacity_dictionary = ref!(:Type => :ExtGState,
-                                  :CA   => :stroke_opacity,
-                                  :ca   => :opacity
+                                  :CA   => stroke_opacity,
+                                  :ca   => opacity
                                   )
         
-        opacity_dictionary_name = "T#{next_opacity_dictionary_id}"
-        opacity_dictionary_registry[key] << { :name => dictionary_name, :obj => opacity_dictionary }
-        # /Resources must include /ExtGState << /TranspLib1 N 0 R
-        # /TranspLib2 N2 0 R /TranspLibN NN 0 R >>
-
-        # need an object << /Type /ExtGState
-        #                   /CA stroke opacity
-        #                   /CA non-stroke-opacity
-
-        # everywhere a dictionary is used, we write "/TranspLibN gs"
+        opacity_dictionary_name = "Tr#{next_opacity_dictionary_id}"
+        opacity_dictionary_registry[key] = { :name => opacity_dictionary_name, :obj => opacity_dictionary }
+        page_extgstates.merge!(opacity_dictionary_name => opacity_dictionary)
       end
+
+      # push a new graphics context onto the graphics context stack
+      add_content "q"
+      add_content "/#{opacity_dictionary_name} gs"
     end
 
     private
--- spec/transparency_spec.rb
+++ spec/transparency_spec.rb
@@ -10,16 +10,16 @@ describe "Document with transparency" do
   
   it "a new extended graphics state should be created for each unique transparency setting" do
     create_pdf
-    @pdf.set_opacity(0.5, :stroke => 0.2)
-    @pdf.set_opacity(0.5, :stroke => 0.75)
+    @pdf.set_opacity(0.5, 0.2)
+    @pdf.set_opacity(0.5, 0.75)
     extgstates = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates
     extgstates.length.should == 2
   end
   
   it "a new extended graphics state should not be created for each duplicate transparency setting" do
     create_pdf
-    @pdf.set_opacity(0.5, :stroke => 0.75)
-    @pdf.set_opacity(0.5, :stroke => 0.75)
+    @pdf.set_opacity(0.5, 0.75)
+    @pdf.set_opacity(0.5, 0.75)
     extgstates = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates
     extgstates.length.should == 1
   end
@@ -28,23 +28,29 @@ describe "Document with transparency" do
     create_pdf
     @pdf.set_opacity(0.5)
     extgstate = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates[0]
-    extgstate.opacity.should == 0.5
-    extgstate.stroke_opacity.should == 0.5
+    extgstate[:opacity].should == 0.5
+    extgstate[:stroke_opacity].should == 0.5
   end
 
   it "setting the transparency with a numerical parameter and a :stroke should set the fill transparency to the numerical parameter and the stroke transparency to the option" do
     create_pdf
-    @pdf.set_opacity(0.5, :stroke => 0.2)
+    @pdf.set_opacity(0.5, 0.2)
     extgstate = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates[0]
-    extgstate.opacity.should == 0.5
-    extgstate.stroke_opacity.should == 0.2
+    extgstate[:opacity].should == 0.5
+    extgstate[:stroke_opacity].should == 0.2
   end
-
-  it "the extended graphics state should reference the transparency library object id" do
-    create_pdf
-    @pdf.set_opacity(0.5, :stroke => 0.2)
-    extgstate = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates[0]
-    transparency_library = PDF::Inspector::TransparencyLibrary.analyze(@pdf.render).transparency_libraries[0]
-    extgstate.child_object_id.should == transparency_library.object_id
+  
+  describe "with more than one page" do
+    it "transparency settings set on the first page should be accessible from subsequent pages without creating an additional extended graphic state" do
+      create_pdf
+      @pdf.set_opacity(0.5, 0.2)
+      @pdf.start_new_page
+      @pdf.set_opacity(0.5, 0.2)
+      extgstates = PDF::Inspector::ExtGState.analyze(@pdf.render).extgstates
+      extgstate = extgstates[0]
+      extgstates.length.should == 1
+      extgstate[:opacity].should == 0.5
+      extgstate[:stroke_opacity].should == 0.2
+    end
   end
 end
