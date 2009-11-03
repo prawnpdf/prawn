29d1ac5fef81e93b56a182e1d2fdeb761e1cf10a
Gregory Brown <gregory.t.brown@gmail.com>
2009-11-03 13:42:49 -0500

    whitespace cleanup

--- lib/prawn/stamp.rb
+++ lib/prawn/stamp.rb
@@ -20,38 +20,54 @@ module Prawn
       end
       
       add_content "q"
+
       x,y = point
       translate_position = "1 0 0 1 %.3f %.3f cm" % [x, y]
       add_content translate_position
-      stamp_dictionary_name = stamp_dictionary_registry[user_defined_name][:stamp_dictionary_name]
-      stamp_dictionary = stamp_dictionary_registry[user_defined_name][:stamp_dictionary]
+      dict = stamp_dictionary_registry[user_defined_name]
+
+      stamp_dictionary_name = dict[:stamp_dictionary_name]
+      stamp_dictionary = dict[:stamp_dictionary]
+
       add_content "/#{stamp_dictionary_name} Do"
       add_content "Q"
+
       page_xobjects.merge!(stamp_dictionary_name => stamp_dictionary)
     end
     
     def create_stamp(user_defined_name="", &block)
+
       raise Prawn::Errors::InvalidName if user_defined_name.empty?
+
       if stamp_dictionary_registry[user_defined_name]
         raise Prawn::Errors::NameTaken
       end
+
       stamp_dictionary = ref!(:Type    => :XObject,
                               :Subtype => :Form,
                               :BBox => [0, 0, bounds.width, bounds.height])
+
       stamp_dictionary_name = "Stamp#{next_stamp_dictionary_id}"
-      stamp_dictionary_registry[user_defined_name] = { :stamp_dictionary_name =>  stamp_dictionary_name, :stamp_dictionary => stamp_dictionary}
+
+      stamp_dictionary_registry[user_defined_name] = 
+        { :stamp_dictionary_name => stamp_dictionary_name, 
+          :stamp_dictionary      => stamp_dictionary}
 
       
       @active_stamp_stream = ""
       @active_stamp_dictionary = stamp_dictionary
+
       yield if block_given?
+
       stamp_dictionary.data[:Length] = @active_stamp_stream.length + 1
       stamp_dictionary << @active_stamp_stream
+
       @active_stamp_stream = nil
       # The ProcSet needs to be assigned at the page level
       procs = @active_stamp_dictionary.data[:ProcSet]
       @active_stamp_dictionary.data.delete(:ProcSet)
       @active_stamp_dictionary = nil
+
       # The ProcSet needs to be assigned at the page level
       proc_set(procs) if procs
     end
