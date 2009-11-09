3c0253b3a9fce13d04235c8ecd8bb288ac5a90b7
Brad Ediger <brad.ediger@madriska.com>
2009-11-09 14:12:31 -0600

    Use vendored prawn-core to run examples

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -1,6 +1,10 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 
+                             %w[.. vendor prawn-core lib]))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require "rubygems"
-require "prawn"   
+
+require "prawn/core"
 require "prawn/layout"
 
 Prawn.debug = true
