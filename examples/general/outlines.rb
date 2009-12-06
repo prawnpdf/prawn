987dc12bb39919037be3f5c98fb097510ed18c54
jonathangreenberg <greenberg@entryway.net>
2009-12-06 14:06:52 -0500

    A better DSL for outlines using blocks instead of Arrays and Hashses

--- examples/general/outlines.rb
+++ examples/general/outlines.rb
@@ -7,18 +7,24 @@ require "#{File.dirname(__FILE__)}/../example_helper.rb"
 
 
 Prawn::Document.generate('outlines.pdf') do
-  text "This is the first Chapter"
+  text "Page 1. This is the first Chapter. "
   start_new_page
-  text "This is the second Chapter"
-  generate_outline( 
-    {
-      ['Chapter 1', 0] => [
-        ['Page 1', 0]
-      ]
-    }, 
-    {
-      ['Chapter 2', 1] => [
-        ['Page 2', 1]
-      ]
-    })
+  text "Page 2. More in the first Chapter. "
+  start_new_page
+  text "Page 3. This is the second Chapter. It has a subsection. "
+  start_new_page
+  text  "Page 4. More in the second Chapter. "
+  generate_outline do
+    section ['Chapter 1', 0] do 
+      page ['Page 1', 0]
+      page ['Page 2', 1]
+    end
+    section ['Chapter 2', 2] do 
+      section ['Chapter 2 Subsection', 2] do
+        page ['Page 3', 2]
+      end
+      page ['Page 4', 3]
+    end
+  end
+  
 end
--- lib/prawn/document/outline.rb
+++ lib/prawn/document/outline.rb
@@ -6,36 +6,64 @@
 module Prawn
   class Document #:nodoc:
     module Outline
-      def generate_outline(*outline)
+      
+      attr_accessor :parent
+      attr_accessor :prev
+      
+      def generate_outline(&block)
         root_outline = ref!(:Type => :Outlines, :Count => 0)
         @store.root.data.merge!({:Outlines => root_outline})
-        generate_child_items(root_outline, outline)
+        @parent = root_outline
+        @prev = nil
+        block.call if block
+      end
+  
+  private
+      
+      def section(item_array, &block)
+        outline_item = create_outline_item(item_array)
+        set_relations(outline_item)
+        increase_count
+        set_variables_for_block(outline_item, block)
+        block.call if block
+        reset_parent(outline_item)
+      end 
+      
+      alias :page :section
+      
+      def create_outline_item(item_array)
+        outline_item = {:Title => Prawn::LiteralString.new(item_array[0]), 
+                          :Parent => parent, :Count => 0}
+        outline_item.merge!(:Dest => [@store.pages.data[:Kids][item_array[1]], 
+                            :Fit]) if item_array[1]
+        outline_item.merge!({:Prev => prev}) if prev
+        outline_item = ref!(outline_item)
       end
       
-      def generate_child_items(parent, child_items)
-        prev = nil
-        child_items.each_with_index do |item, index|
-          item_array = has_children?(item) ? item.keys.first : item
-          outline_item = {:Title => Prawn::LiteralString.new(item_array[0]), 
-                          :Parent => parent, :Count => 0}
-          outline_item.merge!(:Dest => [@store.pages.data[:Kids][item_array[1]], :Fit]) if item_array[1]
-          outline_item.merge!({:Prev => prev}) if prev
-          outline_item = ref!(outline_item)
-          prev.data.merge!({:Next => outline_item}) if prev
-          if index == 0 
-            outline_item.data[:Parent].data.merge!({:First => outline_item})
-          end
-          if index == child_items.count - 1
-            outline_item.data[:Parent].data.merge!({:Last => outline_item})
-          end
-          outline_item.data[:Parent].data[:Count] += 1
-          prev = outline_item
-          generate_child_items(outline_item, item.values.first) if has_children?(item)
-        end
-      end  
+      def set_relations(outline_item)
+        prev.data.merge!({:Next => outline_item}) if prev
+        parent.data.merge!({:First => outline_item}) unless prev
+        parent.data.merge!({:Last => outline_item})
+      end
       
-      def has_children?(item)
-        item.is_a? Hash
+      def increase_count
+        counting_parent = parent
+        while counting_parent 
+          counting_parent.data[:Count] += 1
+          counting_parent = counting_parent.data[:Parent]
+        end
+      end
+      
+      def set_variables_for_block(outline_item, block)
+        @prev = block ? nil : outline_item
+        @parent = outline_item if block
+      end
+      
+      def reset_parent(outline_item)
+        if parent == outline_item
+          @prev = outline_item
+          @parent = outline_item.data[:Parent]
+        end
       end
       
     end
