38e2abc9a471173181b842312774b1654a4f86cf
Brad Ediger <brad.ediger@madriska.com>
2009-12-11 22:02:25 -0600

    Added silly cell example

--- examples/table/cell.rb
+++ examples/table/cell.rb
@@ -8,32 +8,5 @@
 require "#{File.dirname(__FILE__)}/../example_helper.rb"
 
 Prawn::Document.generate("cell.pdf") do 
-  cell = Prawn::Table::Cell.new(
-    :border_width => 3, :padding => 10, :width => 75,
-    :text => "You know that kittens are made of mud!", :document => self)
-  cell2 = Prawn::Table::Cell.new(
-    :border_width => 3, :padding => 10,
-    :text => "And that puppies are made of gravy", :document => self, :font_size => 9)
-  cell3 = Prawn::Table::Cell.new(
-    :border_width => 3, :padding => 10, :width => 100,
-    :text => "This is simply the way of the world", :document => self, :font_style => :bold)
-
-    3.times do
-      cellblock = Prawn::Table::CellBlock.new(self)
-      cellblock << cell << cell2 << cell3
-      cellblock.draw
-    end
-    
-  move_down(20)  
-    
-  cellblock = Prawn::Table::CellBlock.new(self)
-  cellblock << Prawn::Table::Cell.new(
-    :border_width => 3, 
-    :padding => 10, 
-    :borders => [:left, :top],
-    :width => 100, 
-    :text => "This is simply the way of the world", :document => self)
-  cellblock.draw
-
-  stroke_line [100,100], [200,200]
+  cell :content => "test"
 end
