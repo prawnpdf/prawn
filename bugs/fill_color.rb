30f9fac0d952c9db19ce2d7b9288cb1ffb8b4b0a
Gregory Brown <gregory.t.brown@gmail.com>
2010-01-05 08:45:28 -0500

    Filing bug regarding fill_color in tables

---
+++ bugs/fill_color.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+#
+# As of 9e48a6 (2009.01.03), this code fails to recognize fill_color in tables.
+#
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require "prawn/core"
+require "prawn/layout"
+
+Prawn::Document.generate("fill_color.pdf") do
+  fill_color "ff0000"
+  table [%w[1 2 3],%w[4 5 6],%w[7 8 9]], 
+end
+
