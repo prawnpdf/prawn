8807dfb6133a5b8a9b7c2dc372a6b917c91af876
Brad Ediger <brad.ediger@madriska.com>
2010-01-13 18:41:58 -0600

    Shelved transaction-rollback bug

--- bugs/resolved/transaction_page_number_issue_79.rb
+++ bugs/resolved/transaction_page_number_issue_79.rb
@@ -1,6 +1,8 @@
 # As of 2010.01.12, we have confirmed that page_number is not properly set on
 # transaction rollback, resulting in an error from the code sample below.
 #
+# Resolved in 7c62bbf.
+#
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..','lib')
 require "prawn/core"
