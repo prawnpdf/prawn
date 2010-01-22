9a64991453528c4585846e228945cb2a42efcd91
Brad Ediger <brad.ediger@madriska.com>
2010-01-22 12:49:08 -0600

    {100 lbs of crap in 50 lb bag}.should.raise
    Raise Prawn::Errors::CannotFit if Text::Box is asked to draw some text
    into an area that isn't wide enough to draw at least one character.

--- lib/prawn/text/box.rb
+++ lib/prawn/text/box.rb
@@ -234,6 +234,11 @@ module Prawn
                                            :kerning => @kerning,
                                            :size => @font_size,
                                            :width => @width)
+
+          if line_to_print.empty? && remaining_text.length > 0
+            raise Errors::CannotFit
+          end
+
           remaining_text = remaining_text.slice(line_to_print.length..
                                                 remaining_text.length)
           print_ellipses = (@overflow == :ellipses && last_line? &&
