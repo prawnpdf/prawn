b37b1888a0bba02a7a26c656b8bc5280d0bcf2f7
Daniel Nelson <daniel@Optiplex760.(none)>
2010-02-15 17:04:23 -0600

    ensure 1.9.1 runs with test-unit 1.2.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,17 @@
 puts "Prawn specs: Running on Ruby Version: #{RUBY_VERSION}"
 
 require "rubygems"
-require "test/spec"                                                
-require "mocha"
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib') 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'vendor','pdf-inspector','lib')
 require "prawn/core"
 
 Prawn.debug = true
+
+ruby_19 do
+ gem "test-unit", "=1.2.3"
+end
+require "test/spec"
+require "mocha"
 
 gem 'pdf-reader', ">=0.8"
 require "pdf/reader"          
