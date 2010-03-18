ad0a8dcf852fc82d8f818487b8f3c58ad457e758
Daniel Nelson <daniel@Optiplex760.(none)>
2010-03-18 14:18:27 -0500

    improved api documentation for #text

--- lib/prawn/text.rb
+++ lib/prawn/text.rb
@@ -72,17 +72,27 @@ module Prawn
     #      <tt>\<sub></sub></tt>:: subscript
     #      <tt>\<sup></sup></tt>:: superscript
     #      <tt>\<font></font></tt>::
-    #          with the following attributes
-    #            <tt>size="24" or size='24'</tt>::
+    #          with the following attributes (using double or single quotes)
+    #            <tt>size="24"</tt>::
     #                attribute for setting size
-    #            <tt>name="Helvetica" or name='Helvetica'</tt>::
+    #            <tt>name="Helvetica"</tt>::
     #                attribute for setting the font. The font name must be an
     #                AFM font with the desired faces or must be a font that is
     #                already registered using Prawn::Document#font_families
     #      <tt>\<color></color></tt>::
     #          with the following attributes
-    #            <tt>rgb="ffffff" or rgb='ffffff' or rgb="#ffffff" or rgb='#ffffff'</tt>::
-    #            <tt>c="100" m="100" y="100" k="100" or c="100" m="100" y="100" k="100"</tt>::
+    #            <tt>rgb="ffffff" or rgb="#ffffff"</tt>::
+    #            <tt>c="100" m="100" y="100" k="100"</tt>::
+    #      <tt>\<link></link></tt>::
+    #          with the following attributes
+    #            <tt>href="http://example.com"</tt>:: an external link
+    #            <tt>anchor="ToC"</tt>::
+    #                where the value of the anchor attribute is the name of a
+    #                destination that has already been or will be registered
+    #                using Prawn::Core::Destinations#add_dest. A clickable link
+    #                will be created to that destination. 
+    #          Note that you must explicitly underline and color using the
+    #          appropriate tags if you which to draw attention to the link
     #
     # <tt>:kerning</tt>:: <tt>boolean</tt>. Whether or not to use kerning (if it
     #                     is available with the current font) [true]
