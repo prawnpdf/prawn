bc01a7d967433b7df94cc8907872ce2053751f1a
Brad Ediger <brad.ediger@madriska.com>
2010-06-02 00:01:24 -0500

    Properly encode text strings with non-BMP Unicode chars
    Perform correct UTF-16 encoding, creating surrogate pairs as necessary,
    for text strings with non-Basic Multilingual Plane characters.
    
    Closes #120.

--- lib/prawn/core/pdf_object.rb
+++ lib/prawn/core/pdf_object.rb
@@ -13,10 +13,30 @@ module Prawn
                                              
     module_function
 
-    # Treat the Unicode byte-order mark as a raw byte stream so that it can be
-    # prepended to big-endian UTF-16 data below.
-    ruby_18 { ByteOrderMark = "\xFE\xFF" }
-    ruby_19 { ByteOrderMark = "\xFE\xFF".force_encoding("ASCII-8BIT") }
+    ruby_18 do
+      def utf8_to_utf16(str)
+        utf16 = "\xFE\xFF"
+
+        str.unpack("U*").each do |cp|
+          if cp < 0x10000 # Basic Multilingual Plane
+            utf16 << [cp].pack("n")
+          else
+            # pull out high/low 10 bits
+            hi, lo = (cp - 0x10000).divmod(2**10)
+            # encode a surrogate pair
+            utf16 << [0xD800 + hi, 0xDC00 + lo].pack("n*")
+          end
+        end
+
+        utf16
+      end
+    end
+
+    ruby_19 do
+      def utf8_to_utf16(str)
+        "\xFE\xFF".force_encoding("UTF-16BE") + str.encode("UTF-16BE")
+      end
+    end
       
     # Serializes Ruby objects to their PDF equivalents.  Most primitive objects
     # will work as expected, but please note that Name objects are represented 
@@ -50,10 +70,7 @@ module Prawn
       when Prawn::Core::ByteString
         "<" << obj.unpack("H*").first << ">"
       when String
-        unless in_content_stream
-          # Pack string as UTF-16 with a BOM
-          obj = ByteOrderMark + obj.unpack("U*").pack("n*")
-        end
+        obj = utf8_to_utf16(obj) unless in_content_stream
         "<" << obj.unpack("H*").first << ">"
        when Symbol                                                         
          "/" + obj.to_s.unpack("C*").map { |n|
