e5d0773faf1b5a81c19f78b6a5f5cd75933147b1
Felipe Doria <felipe.doria@gmail.com>
2010-09-11 21:20:38 -0300

    Loading examples directly from .rb files

--- new_examples/example_helper.rb
+++ new_examples/example_helper.rb
@@ -1,3 +1,8 @@
+# encoding: utf-8
+#
+# Helper for organizing examples
+#
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'prawn'
@@ -7,51 +12,20 @@ require 'prawn/layout'
 
 Prawn.debug = true
 
-module Snippet
-  def snippet(&block)
+module Examples
+  
+  def load_example(filename)
+    example_source = File.read(filename)
+    
     bounding_box([bounds.left+10, cursor-10], :width => bounds.width-20) do
       font('Courier') do
-        text snippet_source(block.to_s)
+        text example_source.gsub(' ', "\302\240")
       end
     end
     
-    yield
+    eval example_source
   end
   
-  def snippet_source(block_data)
-    file_name, line_number = Helper.parse_block_data(block_data)
-    
-    @examples ||= {file_name => File.readlines(file_name)}
-    @examples[file_name] ||= File.readlines(file_name)
-    
-    snippet_end_line = @examples[file_name][line_number-1].sub(/snippet.*/, 'end')
-    extra_spaces = @examples[file_name][line_number][/\s+/].length
-    
-    i = line_number
-    output = ""
-    while (line = @examples[file_name][i]) != snippet_end_line do
-      output << Helper.use_non_breaking_spaces(Helper.remove_extra_spaces(line, extra_spaces))
-      i += 1
-    end
-    output
-  end
-  
-  module Helper
-    extend self
-    
-    def parse_block_data(block_data)
-      file_name, line_number = block_data.split('@').last.split(':')
-      [file_name, line_number.to_i]
-    end
-    
-    def remove_extra_spaces(string, quantity)
-      string.sub(' '*quantity, '')
-    end
-
-    def use_non_breaking_spaces(string)
-      string.gsub(' ', "\302\240")
-    end
-  end
 end
 
-Prawn::Document.extensions << Snippet
+Prawn::Document.extensions << Examples
---
+++ new_examples/graphics/rounded_polygons.rb
@@ -0,0 +1,14 @@
+def radian(degree)
+  Math::PI/180*degree
+end
+
+def point_on_circle(center, radius, degrees)
+  [center[0] + radius*(Math.cos(radian(degrees))), center[1] - radius*(Math.sin(radian(degrees)))]
+end
+
+pentagon_points = (0..4).map{|i| point_on_circle([200, 400], 100, i * 72)}
+pentagram_points = [0, 2, 4, 1, 3].map{|i| pentagon_points[i]}
+stroke_rounded_polygon(20, *pentagram_points)
+fill_and_stroke_rounded_polygon(10, [100, 250], [200, 300], [300, 250],
+                 [300, 150], [200, 100], [100, 150])
+
