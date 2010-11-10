2ff131a686b3fef3f63ae58e91c6e52cb9707be4
James Healy <jimmy@deefa.com>
2010-11-09 23:32:46 -0600

    update text_rendering_mode to use symbols instead of magic numbers

--- spec/text_rendering_mode_spec.rb
+++ spec/text_rendering_mode_spec.rb
@@ -5,7 +5,7 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")
 describe "#text_rendering_mode" do
   it "should draw the text rendering mode to the document" do
     create_pdf
-    @pdf.text_rendering_mode(1) do
+    @pdf.text_rendering_mode(:stroke) do
       @pdf.text("hello world")
     end
     contents = PDF::Inspector::Text.analyze(@pdf.render)
@@ -14,7 +14,7 @@ describe "#text_rendering_mode" do
   it "should not draw the text rendering mode to the document" +
     " when the new mode matches the old" do
     create_pdf
-    @pdf.text_rendering_mode(0) do
+    @pdf.text_rendering_mode(:fill) do
       @pdf.text("hello world")
     end
     contents = PDF::Inspector::Text.analyze(@pdf.render)
@@ -22,7 +22,7 @@ describe "#text_rendering_mode" do
   end
   it "should restore character spacing to 0" do
     create_pdf
-    @pdf.text_rendering_mode(1) do
+    @pdf.text_rendering_mode(:stroke) do
       @pdf.text("hello world")
     end
     contents = PDF::Inspector::Text.analyze(@pdf.render)
@@ -30,15 +30,16 @@ describe "#text_rendering_mode" do
   end
   it "should function as an accessor when no parameter given" do
     create_pdf
-    @pdf.text_rendering_mode(2) do
+    @pdf.text_rendering_mode(:fill_stroke) do
       @pdf.text("hello world")
-      @pdf.text_rendering_mode.should == 2
+      @pdf.text_rendering_mode.should == :fill_stroke
     end
-    @pdf.text_rendering_mode.should == 0
+    @pdf.text_rendering_mode.should == :fill
   end
-  it "should raise an exception when passed a value < 0 or > 7" do
+  it "should raise an exception when passed an invalid mode" do
     create_pdf
-    lambda { @pdf.text_rendering_mode(-1) }.should raise_error(ArgumentError)
-    lambda { @pdf.text_rendering_mode(8) }.should  raise_error(ArgumentError)
+    lambda { @pdf.text_rendering_mode(-1) }.should.raise(ArgumentError)
+    lambda { @pdf.text_rendering_mode(8) }.should.raise(ArgumentError)
+    lambda { @pdf.text_rendering_mode(:flil) }.should.raise(ArgumentError)
   end
 end
