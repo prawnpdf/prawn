7cce1ea2312a176500bb13e65f679d1aa954bb36
Felipe Doria <felipe.doria@gmail.com>
2010-11-18 17:15:21 -0300

    Moved the text flow example directly onto free flowing text.

--- new_examples/text/free_flowing_text.rb
+++ new_examples/text/free_flowing_text.rb
@@ -10,6 +10,9 @@
 # The text will start being rendered on the current cursor position. When it
 # finishes rendering, the cursor is left directly below the text.
 #
+# This example also show text flowing accross pages folowing the margin box and
+# other bounding boxes.
+#
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
 
@@ -21,10 +24,23 @@ Prawn::Example.generate(filename) do
   move_down 50
   text "This line will go just after we moved the cursor down."
   
-  bounding_box [100, 250], :width => 200, :height => 200 do
-    text "This text will flow along this bounding box we created for it. " * 5
+  move_cursor_to 50
+  text "This text will flow to the next page. " * 20
+  
+  y_position = cursor - 50
+  bounding_box [0, y_position], :width => 200, :height => 150 do
     transparent(0.5) { stroke_bounds }
+    text "This text will flow along this bounding box we created for it. " * 5
   end
   
-  text "And this line will go directly below the bounding box."
+  bounding_box [300, y_position], :width => 200, :height => 150 do
+    transparent(0.5) { stroke_bounds }
+    text "Now look what happens when the free flowing text reaches the end " +
+         "of a bounding box that is narrower than the margin box." +
+         " . " * 200 +
+         "It continues on the next page as if the previous bounding box " +
+         "was cloned. If we want it to have the same border as the one on " +
+         "the previous page we will need to stroke the rectangle again."
+    transparent(0.5) { stroke_bounds }
+  end
 end
