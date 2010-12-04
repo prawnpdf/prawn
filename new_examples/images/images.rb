c6aa032d2d8983b392a8ebe92ae9edbafa381985
Felipe Doria <felipe.doria@gmail.com>
2010-12-04 03:28:42 -0300

    Simple image example.

---
+++ new_examples/images/images.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+#
+# Examples for embedding images.
+#
+require File.expand_path(File.join(File.dirname(__FILE__),
+                                   %w[.. example_helper]))
+
+Prawn::Example.generate("images.pdf") do
+  build_package("images", [
+      [ "Basics", [ "plain_image"
+                  ]
+      ]
+    ]
+  )
+end
---
+++ new_examples/images/plain_image.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+#
+# To embed images onto your pdf file there is the <code>image</code> method.
+# It accepts the file path of the image to be loaded and some optional
+# arguments.
+#
+# If only the image path is provided the image will be rendered starting on
+# the cursor position. No manipulation is done with the image even if it doesn't
+# fit entirely on the page like the following snippet.
+#
+require File.expand_path(File.join(File.dirname(__FILE__),
+                                   %w[.. example_helper]))
+
+filename = File.basename(__FILE__).gsub('.rb', '.pdf')
+Prawn::Example.generate(filename) do
+  text "The image will go right below this line of text."
+  image "#{Prawn::BASEDIR}/data/images/pigs.jpg"
+end
