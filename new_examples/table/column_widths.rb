e37a029e51d8d33e4c5b31add8a1cd3c0a604312
Felipe Doria <felipe.doria@gmail.com>
2010-12-27 01:15:22 -0300

    Updated the table column widths example with the hash based syntax.

--- new_examples/table/column_widths.rb
+++ new_examples/table/column_widths.rb
@@ -4,21 +4,27 @@
 # If the end result isn't good, we can override it with some styling.
 #
 # Individual column widths can be set with the <code>:column_widths</code>
-# option. Just provide an array with the sequential width values.
+# option. Just provide an array with the sequential width values for the columns
+# or a hash were each key-value pair represents the column 0-based index and its
+# width.
 #
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
 
 filename = File.basename(__FILE__).gsub('.rb', '.pdf')
 Prawn::Example.generate(filename) do
-  data = [ ["", "", "this is so very looooooooooooooooooooooooooooooong"],
-           ["", "here we have a line that is long but with small words", ""],
-           ["this is not quite as long as the others", "", ""] ]
+  data = [ ["this is not quite as long as the others",
+            "here we have a line that is long but with smaller words",
+            "this is so very looooooooooooooooooooooooooooooong"] ]
   
   text "Prawn trying to guess the column widths"
   table(data)
   move_down 20
   
-  text "Manually setting the column widths"
-  table(data, :column_widths => [140, 160, 240])
+  text "Manually setting all the column widths"
+  table(data, :column_widths => [100, 200, 240])
+  move_down 20
+  
+  text "Setting only the last column width"
+  table(data, :column_widths => {2 => 240})
 end
