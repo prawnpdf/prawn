9be0de893720fa9567bfdc099f562531a74b8822
Felipe Doria <felipe.doria@gmail.com>
2011-02-01 16:14:30 -0300

    Stub for the manual cover

--- manual/manual/cover.rb
+++ manual/manual/cover.rb
@@ -7,5 +7,20 @@ require File.expand_path(File.join(File.dirname(__FILE__),
 
 filename = File.basename(__FILE__).gsub('.rb', '.pdf')
 Prawn::Example.generate(filename) do
-  text "Prawn by Example", :size => 40
+  move_down 200
+
+  image "#{Prawn::BASEDIR}/data/images/prawn.png",
+        :scale => 0.9,
+        :at => [10, cursor]
+        
+  formatted_text_box([ {:text => "Prawn\n",
+                        :styles => [:bold],
+                        :size => 100}
+                     ], :at => [170, cursor - 50])
+
+  formatted_text_box([ {:text => "by example",
+                        :font => 'Courier',
+                        :size => 60}
+                     ], :at => [170, cursor - 160])
+  
 end
