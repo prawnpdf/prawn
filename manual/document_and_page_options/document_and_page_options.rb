b7d48abf08109d2db5987920822ceef9a136e59f
Felipe Doria <felipe.doria@gmail.com>
2011-02-06 01:42:30 -0300

    Page margins example

--- manual/document_and_page_options/document_and_page_options.rb
+++ manual/document_and_page_options/document_and_page_options.rb
@@ -7,7 +7,8 @@ require File.expand_path(File.join(File.dirname(__FILE__),
 
 Prawn::Example.generate("document_and_page_options.pdf") do
   build_package("document_and_page_options", [
-      {:name => "page_size", :eval_source => false, :full_source => true}
+      {:name => "page_size",    :eval_source => false, :full_source => true},
+      {:name => "page_margins", :eval_source => false, :full_source => true}
     ]
     
   ) do
