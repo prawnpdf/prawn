8eb9baa9073f73b842da27fba297f616cf69adc2
Michael Witrant <mike@lepton.fr>
2011-02-27 13:33:07 +0100

    removed examples covered by manual

--- examples/security/hello_foo.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__),
-                                   %w[.. example_helper]))
-
-Prawn::Document.generate("security_hello_foo.pdf") do
-  text "Hello, world!"
-  encrypt_document :user_password => 'foo', :owner_password => 'bar',
-    :permissions => { :print_document => false }
-end
-
