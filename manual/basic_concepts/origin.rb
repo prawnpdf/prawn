2fed7d0528a3fc970d96c39c7dcd4323cb2bcc00
basti <sk@dotob.de>
2011-11-07 14:05:39 +0100

    * wrong position for sample (should be 100, 100 as origin)

--- manual/basic_concepts/origin.rb
+++ manual/basic_concepts/origin.rb
@@ -28,7 +28,7 @@ Prawn::Example.generate(filename) do
   
   stroke_circle [0, 0], 10
   
-  bounding_box [100, 300], :width => 300, :height => 200 do
+  bounding_box [100, 100], :width => 300, :height => 200 do
     stroke_bounds
     stroke_circle [0, 0], 10
   end
