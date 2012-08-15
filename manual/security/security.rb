7cc912cd50efe35413eb9319b93956aa733628a1
Felipe Doria <felipe.doria@gmail.com>
2012-08-15 00:48:21 -0300

    Split the security encryption example in two

--- manual/security/security.rb
+++ manual/security/security.rb
@@ -9,7 +9,8 @@ Prawn::Example.generate("security.pdf", :page_size => "FOLIO") do
   
   package "security" do |p|
     
-    p.example "encryption", :eval_source => false, :full_source => true
+    p.example "encryption",  :eval_source => false, :full_source => true
+    p.example "permissions", :eval_source => false, :full_source => true
     
     p.intro do
       prose("Security lets you control who can read the document by defining a password.
