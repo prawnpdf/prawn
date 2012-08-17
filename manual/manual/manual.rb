f30d859956e96918e5aa97c03b83bc2bb1c6dafb
Brad Ediger <brad.ediger@madriska.com>
2012-08-17 13:16:40 -0500

    Default to options that yield a smaller manual

--- manual/manual/manual.rb
+++ manual/manual/manual.rb
@@ -9,6 +9,8 @@ require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
 
 Prawn::Example.generate("manual.pdf",
+                        :optimize_objects => true,
+                        :compress => true,
                         :skip_page_creation => true,
                         :page_size => "FOLIO") do
   
