a3ef49d146e68db99d77504d08008e461a2749d7
Pierre Basile <pierre.basile@gmail.com>
2012-08-30 17:17:54 +0200

    add per_page_number_rows features

---
+++ manual/table/per_page_number_rows.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+#
+# To know how many rows are display per page, you can use <code>per_page_number_rows</code>
+# It will be easier to display total per page
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__),
+                                   %w[.. example_helper]))
+
+filename = File.basename(__FILE__).gsub('.rb', '.pdf')
+Prawn::Example.generate(filename) do
+  data = [["The quick brown fox jumped over the lazy dogs."]] * 100
+
+  t = table data
+  t.per_page_number_rows
+
+  # Result is : [33,33,33,1]
+  # 33 is the number of rows in first page
+  # 33 is the number of rows in second page
+  # 33 is the number of rows in thirs page
+  # 1 is the number of rows in the last page
+
+
+  # Now you can display total in bottom of each page
+  pdf.page_count.times do |i|
+    pdf.go_to_page(i+1)
+    total_price = data[i*t.per_page_number_rows[i],(i+1)*t.per_page_number_rows[i]].sum{|s| s[5]}
+    pdf.text "Total Price : #{total_price}"
+  end
+
+end
+
+
+
