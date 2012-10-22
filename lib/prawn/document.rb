419eca8a820862885e8693a3710f75320f922abc
jonathangreenberg <greenberg@entryway.net>
2012-10-22 11:38:39 -0400

    Index IO and ObjectHash to prevent excessive pdf sizes
    - use object_id for filename or streams as index keys

--- lib/prawn/document.rb
+++ lib/prawn/document.rb
@@ -241,6 +241,9 @@ module Prawn
     #
     #  pdf.start_new_page(:template => multipage_template.pdf, :template_page => 2)
     #
+    # Note: templates get indexed by either the object_id of the filename or stream
+    # entered so that if you reuse the same template multiple times be sure to use the
+    # same instance for more efficient use of resources and smaller rendered pdfs.
     def start_new_page(options = {})
       if last_page = state.page
         last_page_size    = last_page.size
@@ -539,15 +542,15 @@ module Prawn
     # through existing pages after they are created.
     #
     # Parameters are:
-    # 
-    # <tt>string</tt>:: Template string for page number wording.  
+    #
+    # <tt>string</tt>:: Template string for page number wording.
     #                   Should include '<page>' and, optionally, '<total>'.
     # <tt>options</tt>:: A hash for page numbering and text box options.
-    #     <tt>:page_filter</tt>:: A filter to specify which pages to place page numbers on.  
+    #     <tt>:page_filter</tt>:: A filter to specify which pages to place page numbers on.
     #                             Refer to the method 'page_match?'
     #     <tt>:start_count_at</tt>:: The starting count to increment pages from.
     #     <tt>:total_pages</tt>:: If provided, will replace <total> with the value given.
-    #                             Useful to override the total number of pages when using 
+    #                             Useful to override the total number of pages when using
     #                             the start_count_at option.
     #     <tt>:color</tt>:: Text fill color.
     #
@@ -558,7 +561,7 @@ module Prawn
     #          five.
     #
     #   Prawn::Document.generate("page_with_numbering.pdf") do
-    #     number_pages "<page> in a total of <total>", 
+    #     number_pages "<page> in a total of <total>",
     #                                          {:start_count_at => 5,
     #                                           :page_filter => lambda{ |pg| pg != 1 },
     #                                           :at => [bounds.right - 50, 0],
@@ -578,7 +581,7 @@ module Prawn
       txtcolor = opts.delete(:color)
       # An explicit height so that we can draw page numbers in the margins
       opts[:height] = 50 unless opts.has_key?(:height)
-      
+
       start_count = false
       pseudopage = 0
       (1..page_count).each do |p|
@@ -589,7 +592,7 @@ module Prawn
                        else
                          start_count_at.to_i
                        end
-        end        
+        end
         if page_match?(page_filter, p)
           go_to_page(p)
           # have to use fill_color here otherwise text reverts back to default fill color
@@ -598,13 +601,13 @@ module Prawn
           str = string.gsub("<page>","#{pseudopage}").gsub("<total>","#{total_pages}")
           text_box str, opts
           start_count = true  # increment page count as soon as first match found
-        end 
+        end
         pseudopage += 1 if start_count
       end
     end
 
     # Provides a way to execute a block of code repeatedly based on a
-    # page_filter.  
+    # page_filter.
     #
     # Available page filters are:
     #   :all         repeats on every page
@@ -612,7 +615,7 @@ module Prawn
     #   :even        repeats on even pages
     #   some_array   repeats on every page listed in the array
     #   some_range   repeats on every page included in the range
-    #   some_lambda  yields page number and repeats for true return values 
+    #   some_lambda  yields page number and repeats for true return values
     def page_match?(page_filter, page_number)
       case page_filter
       when :all
@@ -626,7 +629,7 @@ module Prawn
       when Proc
         page_filter.call(page_number)
       end
-    end  
+    end
 
 
     # Returns true if content streams will be compressed before rendering,
