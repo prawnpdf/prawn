36f7622f8bc494383f7f65422adff095f72a68a9
Brad Ediger <brad.ediger@madriska.com>
2013-03-08 08:53:22 -0800

    Typo fix

--- manual/manual/foreword.rb
+++ manual/manual/foreword.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-# Prawn manual foreword page. 
+# Prawn manual foreword page.
 #
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
@@ -9,11 +9,11 @@ filename = File.basename(__FILE__).gsub('.rb', '.pdf')
 Prawn::Example.generate(filename) do
   header("Foreword, by Gregory Brown")
   prose %{
-    Back in 2008, the state of PDF generation in 
-    Ruby was grim. The best general purpose tool that existed was 
+    Back in 2008, the state of PDF generation in
+    Ruby was grim. The best general purpose tool that existed was
     Austin Ziegler's PDF::Writer, which was an ambitious but
     painfully unsustainable project. Despite years of hard work
-    from Austin, the code was slow, buggy, and hard to 
+    from Austin, the code was slow, buggy, and hard to
     understand. All of those things made it very difficult
     to extend with the many features that its users needed,
     and so it gradually turned into abandonware.
@@ -30,12 +30,12 @@ Prawn::Example.generate(filename) do
     Ruby 1.9, and would remain painfully slow for many
     ordinary tasks.
 
-    Against my better judgement, but out of desperation, I 
+    Against my better judgement, but out of desperation, I
     was convinced that the only way forward was to attempt the
     big rewrite. It is good that I didn't realize
-    how impossible that task would be, because otherwise, 
+    how impossible that task would be, because otherwise,
     I would have never started working
-    on Prawn. But with the support of dozens of Ruby community 
+    on Prawn. But with the support of dozens of Ruby community
     members who had graciously crowd-funded this project
     so that I could take a few months off of work to
     kick start it (long before Kickstarter existed!), Prawn
@@ -50,13 +50,13 @@ Prawn::Example.generate(filename) do
     nearly every feature of this project. From day one,
     Prawn was a stone soup in which virtually all of the
     tasty bits were provided by the community at large --
-    my most significant contribution was to simply 
+    my most significant contribution was to simply
     keep stirring the pot. Now, the pot mostly stirs itself,
     with the occasional nudge from one of the core
     team members (usually Brad or James, but everyone
     has pitched in at one time or the other.)
 
-    Although Prawn bears no resemblence to PDF::Writer, this library
+    Although Prawn bears no resemblance to PDF::Writer, this library
     is most certainly its spiritual successor. That means that between
     Austin's efforts and that of the Prawn team, we've been
     trying to solve the PDF problem comfortably in Ruby for
