47a81b0c6229e7598e5537794c81af94f588e03e
Thomas Leitner <t_leitner@gmx.at>
2013-09-29 18:30:55 +0200

    Updated the manual to use the (now) built-in stroke_axis method

--- manual/example_helper.rb
+++ manual/example_helper.rb
@@ -382,26 +382,7 @@ module Prawn
     # examples.
     #
     def stroke_axis(options={})
-      options = { :height => (cursor - 20).to_i,
-                  :width => bounds.width.to_i
-                }.merge(options)
-
-      dash(1, :space => 4)
-      stroke_horizontal_line(-21, options[:width], :at => 0)
-      stroke_vertical_line(-21, options[:height], :at => 0)
-      undash
-
-      fill_circle [0, 0], 1
-
-      (100..options[:width]).step(100) do |point|
-        fill_circle [point, 0], 1
-        draw_text point, :at => [point-5, -10], :size => 7
-      end
-
-      (100..options[:height]).step(100) do |point|
-        fill_circle [0, point], 1
-        draw_text point, :at => [-17, point-2], :size => 7
-      end
+      super({:height => (cursor - 20).to_i}.merge(options))
     end
 
     # Reset some of the Prawn settings including graphics and text to their
