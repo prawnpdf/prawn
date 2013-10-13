9ae4c6988913c9eb25ba29dfc1be54f351b06e1d
Thomas Leitner <t_leitner@gmx.at>
2013-10-13 12:04:48 +0200

    Updated dash examples in manual to show new full PDF dash capabilities

--- manual/graphics/stroke_dash.rb
+++ manual/graphics/stroke_dash.rb
@@ -1,14 +1,17 @@
 # encoding: utf-8
 #
-# This sets the dashed pattern for lines and curves.
-#
-# The (dash) length defines how long each dash will be.
+# This sets the dashed pattern for lines and curves. The (dash) length defines
+# how long each dash will be.
 #
 # The <code>:space</code> option defines the length of the space between the
 # dashes.
 #
 # The <code>:phase</code> option defines the start point of the sequence of
-# dashes and spaces. 
+# dashes and spaces.
+#
+# Complex dash patterns can be specified by using an array with alternating
+# dash/gap lengths for the first parameter (note that the <code>:space</code>
+# option is ignored in this case).
 #
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
@@ -16,27 +19,29 @@ require File.expand_path(File.join(File.dirname(__FILE__),
 filename = File.basename(__FILE__).gsub('.rb', '.pdf')
 Prawn::Example.generate(filename) do
   stroke_axis
-  
+
+  dash([1, 2, 3, 2, 1, 5], :phase => 6)
+  stroke_horizontal_line 50, 500, :at => 230
+  dash([1, 2, 3, 4, 5, 6, 7, 8])
+  stroke_horizontal_line 50, 500, :at => 220
+
   base_y = 210
-  
+
   24.times do |i|
     length = (i / 4) + 1
     space = length            # space between dashes same length as dash
     phase = 0                 # start with dash
-    
+
     case i % 4
-    when 0
-      base_y -= 5
-    when 1
-      phase = length          # start with space between dashes
-    when 2
-      space = length * 0.5    # space between dashes half as long as dash
+    when 0 then base_y -= 5
+    when 1 then phase = length        # start with space between dashes
+    when 2 then space = length * 0.5  # space between dashes half as long as dash
     when 3
       space = length * 0.5    # space between dashes half as long as dash
       phase = length          # start with space between dashes
     end
     base_y -= 5
-    
+
     dash(length, :space => space, :phase => phase)
     stroke_horizontal_line 50, 500, :at => base_y - (2 * i)
   end
