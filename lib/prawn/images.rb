20d102a6da2c4835d4a2f4930dc404f65243c0f7
Matt Patterson <matt@reprocessed.org>
2013-11-08 18:24:08 +0100

    Refactor so images via Pathname are read in binmode
    * Separate out image IO opening and verification logic
    * test that embedded images parse correctly (this was the problem that
      was being masked before)
    * Add simple matcher to tidy up checking XObject parseability
    * fixes #570:
    * don't rely on binmode being defined on IO-like objects (re: #585)
    
    Pathname instances respond to #read but not #binmode, and the old code
    was assuming that anything which responded to #read was an IO, with the
    result that Pathname instances were beign treated as IO's and getting
    read called on them, bypassing File.binread (things responding
    to #binmode have that called so with a real IO it would have #read
    called after it had been put into binmode...
    
    The fix is simple enough: treat everything that responds to `rewind` as
    an IO (IO's and Files), and anything else treat as a path, and open it
    'rb'.

--- lib/prawn/images.rb
+++ lib/prawn/images.rb
@@ -76,19 +76,8 @@ module Prawn
     # the given image. Return a pair: [pdf_obj, info].
     #
     def build_image_object(file)
-      # Rewind if the object we're passed is an IO, so that multiple embeds of
-      # the same IO object will work
-      file.rewind  if file.respond_to?(:rewind)
-      # read the file as binary so the size is calculated correctly
-      file.binmode if file.respond_to?(:binmode)
-
-      if file.respond_to?(:read)
-        image_content = file.read
-      else
-        raise ArgumentError, "#{file} not found" unless File.file?(file)  
-        image_content = File.binread(file)
-      end
-      
+      io = verify_and_open_image(file)
+      image_content = io.read
       image_sha1 = Digest::SHA1.hexdigest(image_content)
 
       # if this image has already been embedded, just reuse it
@@ -137,6 +126,25 @@ module Prawn
     end
 
     private
+
+    def verify_and_open_image(io_or_path)
+      # File or IO
+      if io_or_path.respond_to?(:rewind)
+        io = io_or_path
+        # Rewind if the object we're passed is an IO, so that multiple embeds of
+        # the same IO object will work
+        io.rewind
+        # read the file as binary so the size is calculated correctly
+        # guard binmode because some objects acting io-like don't implement it
+        io.binmode if io.respond_to?(:binmode)
+        return io
+      end
+      # String or Pathname
+      io_or_path = Pathname.new(io_or_path)
+      raise ArgumentError, "#{io_or_path} not found" unless io_or_path.file?
+      io = io_or_path.open('rb')
+      io
+    end
 
     def image_position(w,h,options)
       options[:position] ||= :left
