fd3fcc735db90538276e553eb55392d4ec83c58a
Alexander Mankuta <cheba@pointlessone.org>
2013-12-20 18:54:40 +0200

    JPG images optimization
    A few unnecessary reads can cost you a lot.
    
    For a big JPEG (~9 MB) speed up is noticeable.
    
                 user     system      total        real    memory
    Before: 32.770000   0.260000  33.030000 ( 33.491774)  34.5 MB
    After:  19.600000   0.050000  19.650000 ( 19.793003)  29.6 MB
    
    Speed: **1.68x faster**
    Memory: **14% less**
    
                 user     system      total        real   memory
    Before: 37.660000   0.200000  37.860000 ( 38.482895)  33.9 MB
    After:  24.170000   0.060000  24.230000 ( 24.422936)  29.8 MB
    
    Speed: **1.56x faster**
    Memory: **12% less**
    
                    user     system        total         real   memory
    Before: 10487.800000 295.590000 10783.390000 (7131.061000)  528.3 MB (549.9 MB peak)
    After:      7.350000   0.180000     7.530000 (   6.400000)  114.7 MB
    
    Speed: **1,432x faster**
    Memory: **78% less**
    
                 user     system      total        real   memory
    Before: 21.270153   0.239070  21.509223 ( 20.862641)  150.1 Mb
    After:  16.131227   0.186280  16.317507 ( 15.169135)  143.4 MB
    
    Speed: **1.32x faster**
    Memory: **4% less**

--- lib/prawn/images/jpg.rb
+++ lib/prawn/images/jpg.rb
@@ -17,7 +17,7 @@ module Prawn
       attr_reader :width, :height, :bits, :channels
       attr_accessor :scaled_width, :scaled_height
 
-      JPEG_SOF_BLOCKS = %W(\xc0 \xc1 \xc2 \xc3 \xc5 \xc6 \xc7 \xc9 \xca \xcb \xcd \xce \xcf)
+      JPEG_SOF_BLOCKS = [0xC0, 0xC1, 0xC2, 0xC3, 0xC5, 0xC6, 0xC7, 0xC9, 0xCA, 0xCB, 0xCD, 0xCE, 0xCF]
       JPEG_APP_BLOCKS = %W(\xe0 \xe1 \xe2 \xe3 \xe4 \xe5 \xe6 \xe7 \xe8 \xe9 \xea \xeb \xec \xed \xee \xef)
 
       def self.can_render?(image_blob)
@@ -29,22 +29,22 @@ module Prawn
       # <tt>:data</tt>:: A binary string of JPEG data
       #
       def initialize(data)
-        @data = data.dup
-        data.force_encoding(::Encoding::BINARY)
-        data = StringIO.new(data)
+        @data = data
+        d = StringIO.new(@data)
+        d.binmode
 
-        c_marker = "\xff" # Section marker.
-        data.read(2)   # Skip the first two bytes of JPEG identifier.
+        c_marker = 0xff # Section marker.
+        d.seek(2)    # Skip the first two bytes of JPEG identifier.
         loop do
-          marker, code, length = data.read(4).unpack('aan')
+          marker, code, length = d.read(4).unpack('CCn')
           raise "JPEG marker not found!" if marker != c_marker
 
           if JPEG_SOF_BLOCKS.include?(code)
-            @bits, @height, @width, @channels = data.read(6).unpack("CnnC")
+            @bits, @height, @width, @channels = d.read(6).unpack("CnnC")
             break
           end
 
-          data.read(length - 2)
+          d.seek(length - 2, IO::SEEK_CUR)
         end
       end
 
