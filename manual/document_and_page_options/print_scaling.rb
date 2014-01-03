ec43976ba0987f29d014e223bcccff0b9f0077bb
Erik Flood <erik.n.flood@gmail.com>
2014-01-03 18:48:29 +0100

    Update manual with more information about page scaling, based on comment by @jlduran

--- manual/document_and_page_options/print_scaling.rb
+++ manual/document_and_page_options/print_scaling.rb
@@ -1,16 +1,20 @@
 # encoding: utf-8
 #
-# When printing a PDF, many PDF readers automatically scales the content to fit within the page margins. This is not
-# always the wanted behaviour and some PDF readers does not even offer a possibility to disable this feature.
+# (Optional; PDF 1.6) The page scaling option to be selected when a print dialog is displayed for this document.
+# Valid values are <code>None</code>, which indicates that the print dialog should reflect no page scaling,
+# and <code>AppDefault</code>, which indicates that applications should use the current print scaling.
+# If this entry has an unrecognized value, applications should use the current print scaling.
+# Default value: <code>AppDefault</code>.
 #
-# Prawn can disable this feature directly in the PDF document, to disable automatic print scaling just supply
-# <code>:print_scaling => :none</code> when creating a document.
+# Note: If the print dialog is suppressed and its parameters are provided directly by the application, the value of this entry should still be used.
 #
+
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
 
 Prawn::Document.generate("print_scaling.pdf",
-                         :page_layout => :landscape
+                         :page_layout => :landscape,
+                         :print_scaling => :none
 ) do
   text "When you print this document, the scale to fit in print preview should be disabled by default."
 end
