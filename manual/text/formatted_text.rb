c6be9dfab0b190255fc03626b72b3291830aec4e
Gregory Brown <gregory.t.brown@gmail.com>
2014-03-06 14:34:44 -0500

    Remove documentation for anchor because the feature is not fully supported

--- manual/text/formatted_text.rb
+++ manual/text/formatted_text.rb
@@ -17,10 +17,9 @@
 # <code>:character_spacing</code> (additional space between the characters),
 # <code>:font</code> (the name of a registered font), <code>:color</code> (the
 # same input accepted by <code>fill_color</code> and <code>stroke_color</code>),
-# <code>:link</code> (an URL to create a link), <code>:anchor</code> (a
-# destination inside the document), and <code>:local</code> (a link to a local
-# file).
-#
+# <code>:link</code> (an URL to create a link), and <code>:local</code> 
+# (a link to a local file).
+
 require File.expand_path(File.join(File.dirname(__FILE__),
                                    %w[.. example_helper]))
 
@@ -36,9 +35,6 @@ Prawn::Example.generate(filename) do
                    { :text => "Link to the wiki. ",
                      :color => "0000FF",
                      :link => "https://github.com/prawnpdf/prawn/wiki" },
-                   { :text => "Link to the Text Reference. "  ,
-                     :color => "0000FF",
-                     :anchor => "Text Reference" },
                    { :text => "Link to a local file. ",
                      :color => "0000FF",
                      :local => "./local_file.txt" }
