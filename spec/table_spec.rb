36ab64dcc3ecc743fb4ba88142225a78afcaa21f
Hartwig Brandl <mail@hartwigbrandl.at>
2014-03-18 16:48:14 +0100

    adding unresolved tag to failing test cases for issue 438

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -46,7 +46,7 @@ describe "Prawn::Table" do
   end
 
   describe "You can explicitly set the column widths and use a colspan > 1" do
-    it "should render a correct table in a bounding box with given colspans", :issue => 438 do
+    it "should render a correct table in a bounding box with given colspans", :issue => 438, :unresolved do
       pdf = Prawn::Document.new
       data = [  [ {:content => "1", :colspan => 8} ],
                 [1,2,3,4,5,6,7,8],
@@ -64,7 +64,7 @@ describe "Prawn::Table" do
       end
     end
 
-    it "should not throw a CannotFit Error", :issue => 438 do
+    it "should not throw a CannotFit Error", :issue => 438, :unresolved do
       pdf = Prawn::Document.new
       data = [  [ {:content => "1", :colspan => 8} ],
                 [1,2,3,4,5,6,7,8],
