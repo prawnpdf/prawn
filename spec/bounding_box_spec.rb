f8a2d533bc86ade64a4930910d0846435d716c75
Tim Vandecasteele <tim.vandecasteele@gmail.com>
2014-04-23 09:46:52 +0200

    Remember the bounds when a bounding box flows to existing page

--- spec/bounding_box_spec.rb
+++ spec/bounding_box_spec.rb
@@ -507,4 +507,15 @@ describe "BoundingBox#move_past_bottom" do
     pages[0][:strings].should == ["Foo"]
     pages[1][:strings].should == ["Bar"]
   end
+
+  it "should remember the bounding box when going to an existing page" do
+    @pdf.start_new_page
+    @pdf.go_to_page 1
+
+    @pdf.bounding_box([0, @pdf.cursor], :width => 100) do
+      original_bounds = @pdf.bounds
+      @pdf.bounds.move_past_bottom
+      @pdf.bounds.should == original_bounds
+    end
+  end
 end
