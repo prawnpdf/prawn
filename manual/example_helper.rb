eea0e5bdf7f8660bd9bd6b5715d418e27311de63
Gregory Brown <gregory.t.brown@gmail.com>
2014-05-30 12:36:13 -0400

    Require prawn/table in manual

--- manual/example_helper.rb
+++ manual/example_helper.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 
 require_relative "../lib/prawn"
+require_relative "../lib/prawn/table"
 require "prawn/manual_builder"
 
 Prawn::ManualBuilder.manual_dir = File.dirname(__FILE__)
