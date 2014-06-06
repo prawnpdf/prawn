53f797f6e9bb75be79676bfda1cd7292a7af3c0a
Evan Sharp <evan@packetmonkey.org>
2014-06-06 14:40:47 -0400

    Rename 'name' variable to 'src'

--- lib/prawn/font.rb
+++ lib/prawn/font.rb
@@ -279,21 +279,19 @@ module Prawn
     # Shortcut interface for constructing a font object.  Filenames of the form
     # *.ttf will call Font::TTF.new, *.dfont Font::DFont.new, and anything else
     # will be passed through to Font::AFM.new()
-    #
-    def self.load(document, name, options={})
-      # Name is really either a string containing a file path or an IO object
-      case format(document, name, options)
-      when 'ttf'   then TTF.new(document, name, options)
-      when 'dfont' then DFont.new(document, name, options)
-      else AFM.new(document, name, options)
+    def self.load(document, src, options={})
+      case format(document, src, options)
+      when 'ttf'   then TTF.new(document, src, options)
+      when 'dfont' then DFont.new(document, src, options)
+      else AFM.new(document, src, options)
       end
     end
 
-    def self.format(document, name, options)
+    def self.format(document, src, options)
       family = document.font_families.fetch(options[:family]) { {} }
       return family.fetch :format if family.key? :format
 
-      case name.to_s
+      case src.to_s
       when /\.ttf$/i   then return 'ttf'
       when /\.dfont$/i then return 'dfont'
       else return 'afm'
