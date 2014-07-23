576fdf03394d86cf6edb771dcd0042e0d93df266
Gregory Brown <gregory.t.brown@gmail.com>
2014-07-23 09:42:05 -0400

    Remove trailing whitespace

--- manual/table.rb
+++ manual/table.rb
@@ -6,7 +6,7 @@ Prawn::ManualBuilder::Example.generate(filename) do
   header("Prawn::Table")
 
   prose <<-END_TEXT
-    As of Prawn 1.2.0, Prawn::Table has been extracted into its own 
+    As of Prawn 1.2.0, Prawn::Table has been extracted into its own
     semi-officially supported gem.
 
     Please see https://github.com/prawnpdf/prawn-table for more details.
