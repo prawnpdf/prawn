8ce3eaaaa1b81d28acee8dd267b0404d16f0c527
Gregory Brown <gregory.t.brown@gmail.com>
2014-07-30 08:33:52 -0400

    Add Prawn::View

---
+++ lib/prawn/view.rb
@@ -0,0 +1,21 @@
+module Prawn
+  module View
+    def document
+      @document ||= Prawn::Document.new
+    end
+
+    def method_missing(m, *a, &b)
+      document.send(m, *a, &b)
+    rescue NoMethodError
+      super
+    end
+
+    def update(&b)
+      instance_eval(&b)
+    end
+
+    def save_as(filename)
+      document.render_file(filename)
+    end
+  end
+end
