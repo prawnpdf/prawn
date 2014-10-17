766a5343424889b4e08146c35c9c75ed97dc11f5
Mario Albert <Mario.Albert@gmx.de>
2014-10-17 09:08:52 -0400

    Fix issue with soft hyphen width calculations

--- lib/prawn/text/formatted/line_wrap.rb
+++ lib/prawn/text/formatted/line_wrap.rb
@@ -102,6 +102,10 @@ module Prawn
 
               if @accumulated_width + segment_width <= @width
                 @accumulated_width += segment_width
+                if segment[-1] == soft_hyphen
+                  sh_width = @document.width_of("#{soft_hyphen}", :kerning => @kerning)
+                  @accumulated_width -= sh_width
+                end
                 @fragment_output += segment
               else
                 end_of_the_line_reached(segment)
