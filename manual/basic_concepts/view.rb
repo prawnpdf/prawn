b7a38831277d8bd0f3e74225060b2241f52620bf
Giuseppe Bertini <giuseppe@mailserver>
2015-02-03 11:01:08 +0100

    example of a document method added to the view.rb manual page

--- manual/basic_concepts/view.rb
+++ manual/basic_concepts/view.rb
@@ -6,11 +6,17 @@
 # <code>Prawn::Document</code>'s state, and you will avoid accidental method
 # collisions within <code>Prawn::Document</code>.
 #
-# To build custom classes that make use of other custom classes,
-# you can define a method named <code>document()</code> that returns
-# any object that acts similar to a <code>Prawn::Document</code>
-# object. <code>Prawn::View</code> will then direct all delegated
-# calls to that object instead.
+# You would typically define in your custom class a <code>document</code> instance
+# method with a <code>Prawn::Document</code> initialized to your heart's content.
+# This method will be called repeatedly by <code>Prawn::View</code>, so do not
+# forget to assign the object to an instance variable via the <code>||=</code> operator.
+#
+# If you do not define the <code>document</code> method, a <code>Prawn::Document</code>
+# will be lazily instantiated for you, using default initialization settings,
+# such as page size, layout, margins, etc.
+#
+# Either way, your custom objects will have access to all <code>Prawn::Document</code>
+# methods.
 
 require_relative '../example_helper'
 
@@ -19,6 +25,10 @@ class Greeter
 
   def initialize(name)
     @name = name
+  end
+
+  def document
+    @my_prawn_doc ||= Prawn::Document.new(page_size: 'A4')
   end
 
   def say_hello
@@ -33,8 +43,6 @@ class Greeter
 end
 
 greeter = Greeter.new('Gregory')
-
 greeter.say_hello
 greeter.say_goodbye
-
 greeter.save_as('greetings.pdf')
