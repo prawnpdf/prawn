89805b49176d62f0b292f52815d6d70b5833712b
Jamis Buck <jamisbuck@gmail.com>
2017-02-12 16:39:34 -0700

    use new TTFunk::Collection#map method

--- lib/prawn/font/ttc.rb
+++ lib/prawn/font/ttc.rb
@@ -7,30 +7,26 @@ module Prawn
       # Returns a list of the names of all named fonts in the given ttc file.
       # They are returned in order of their appearance in the file.
       #
-      def self.fonts(file)
-        TTFunk::Collection.open(file) do |f|
-          list = []
-
-          f.each do |font|
-            list << font.name.font_name.first
-          end
-
-          list
+      def self.font_names(file)
+        TTFunk::Collection.open(file) do |ttc|
+          ttc.map { |font| font.name.font_name.first }
         end
       end
 
       private
 
       def read_ttf_file
-        TTFunk::File.from_ttc(@name,
-          font_option_to_index(@name, @options[:font]))
+        TTFunk::File.from_ttc(
+          @name,
+          font_option_to_index(@name, @options[:font])
+        )
       end
 
       def font_option_to_index(file, option)
         if option.is_a?(Numeric)
           option
         else
-          self.class.fonts(file).index { |n| n == option } || 0
+          self.class.font_names(file).index { |n| n == option } || 0
         end
       end
     end
