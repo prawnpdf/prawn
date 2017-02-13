e7595b53f04f03e0d1813fd39e3f532a36a062f3
Giuseppe Bertini <giuseppe.bertini@gmail.com>
2017-02-13 15:43:16 +0100

    length of comment lines reduced

--- manual/basic_concepts/view.rb
+++ manual/basic_concepts/view.rb
@@ -1,22 +1,25 @@
-# To create a custom class that extends Prawn's functionality,
-# use the <code>Prawn::View</code> mixin. This approach is safer than creating
-# subclasses of <code>Prawn::Document</code> while being just as convenient.
+# To create a custom class that extends Prawn's functionality, use the
+# <code>Prawn::View</code> mixin. This approach is safer than creating
+# subclasses of <code>Prawn::Document</code> while being just as
+# convenient.
 #
-# By using this mixin, your state will be kept completely separate from
-# <code>Prawn::Document</code>'s state, and you will avoid accidental method
-# collisions within <code>Prawn::Document</code>.
+# By using this mixin, your state will be kept completely separate
+# from <code>Prawn::Document</code>'s state, and you will avoid
+# accidental method collisions within <code>Prawn::Document</code>.
 #
-# You would typically define in your custom class a <code>document</code> instance
-# method with a <code>Prawn::Document</code> initialized to your heart's content.
-# This method will be called repeatedly by <code>Prawn::View</code>, so do not
-# forget to assign the object to an instance variable via the <code>||=</code> operator.
+# You might define in your custom class a <code>document</code>
+# instance method with a <code>Prawn::Document</code> initialized to
+# your heart's content. This method will be called repeatedly by
+# <code>Prawn::View</code>, so do not forget to assign the object to
+# an instance variable via the <code>||=</code> operator.
 #
-# If you do not define the <code>document</code> method, a <code>Prawn::Document</code>
-# will be lazily instantiated for you, using default initialization settings,
-# such as page size, layout, margins, etc.
+# If you do not define the <code>document</code> method, a
+# <code>Prawn::Document</code> will be lazily instantiated for you,
+# using default initialization settings, such as page size, layout,
+# margins, etc.
 #
-# Either way, your custom objects will have access to all <code>Prawn::Document</code>
-# methods.
+# Either way, your custom objects will have access to all
+# <code>Prawn::Document</code> methods.
 
 require_relative '../example_helper'
 
