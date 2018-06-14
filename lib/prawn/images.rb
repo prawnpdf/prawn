5a26de0e1a2021298316df91e119f798c2753136
Oleg Vivtash <oleg@vivtash.net>
2018-06-14 13:57:45 +0300

    Check if image responds to :read

--- lib/prawn/images.rb
+++ lib/prawn/images.rb
@@ -137,11 +137,11 @@ module Prawn
 
     def verify_and_open_image(io_or_path)
       # File or IO
-      if io_or_path.respond_to?(:rewind)
+      if io_or_path.respond_to?(:read)
         io = io_or_path
         # Rewind if the object we're passed is an IO, so that multiple embeds of
         # the same IO object will work
-        io.rewind
+        io.rewind if io.respond_to?(:rewind)
         # read the file as binary so the size is calculated correctly
         # guard binmode because some objects acting io-like don't implement it
         io.binmode if io.respond_to?(:binmode)
