0649a585b7d0d8ae127d62d49daa2c5fad14e570
Benoit Daloze <eregontp@gmail.com>
2020-12-30 15:08:24 +0100

    The manual hash is the same on TruffleRuby and CRuby

--- spec/prawn_manual_spec.rb
+++ spec/prawn_manual_spec.rb
@@ -5,7 +5,7 @@ require 'digest/sha2'
 
 MANUAL_HASH =
   case RUBY_ENGINE
-  when 'ruby'
+  when 'ruby', 'truffleruby'
     'de26db4fe63e024231c0a332203b41305103d877b584a2e98dbd0561bced39f2c066b5c0c96a4686e586a9deb347f099dac4c646446dadb1521a7d4a674ae6fb'
   when 'jruby'
     'c002ffaf6fe4b2877bd2244735e99c04a4b28b06bc365f343411af052d491660e0d858a956a757ad15a4ed16d6808fc8d726fd683d524f5a3f7c0c8b9566b683'
