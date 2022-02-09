e0ed7886a4dc16ccd8708c0aaef0afd0f590f79d
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-02-09 11:34:47 -0800

    Add TruffleRuby head to CI
    This is an clean version of #118 against current master head.  Thanks to @eregon for the original submission

--- spec/prawn_manual_spec.rb
+++ spec/prawn_manual_spec.rb
@@ -4,12 +4,11 @@ require 'spec_helper'
 require 'digest/sha2'
 
 MANUAL_HASH =
-  case RUBY_ENGINE
-  when 'ruby'
-    '2c0279e0bff2a9120494a52aa46216c1871902b5e66a3537bd4d3cbd66db0096b43b6e1ae0e4e189b561c4db9fa1afacb6c41f260e3aaf942faae2fee352d35b'
-  when 'jruby'
-    '51baf6440907e9e38d22f50deafa91572aec1174e621c044ae077cfe3d4361982a505dae5f013dd06f64f38cb9b3a38d5a3f8f0903849591774e298a3c91d39a'
-  end
+  {
+    'ruby' => '2c0279e0bff2a9120494a52aa46216c1871902b5e66a3537bd4d3cbd66db0096b43b6e1ae0e4e189b561c4db9fa1afacb6c41f260e3aaf942faae2fee352d35b',
+    'jruby' => '51baf6440907e9e38d22f50deafa91572aec1174e621c044ae077cfe3d4361982a505dae5f013dd06f64f38cb9b3a38d5a3f8f0903849591774e298a3c91d39a',
+    'truffleruby' => '2d001a18e27582cce60a6610ca0239de96e7413b0c2a657af5ef364cdc793f3ecd22fbf0ca88bd33cbc24f08cbf0f055cc24a9c2661087047b8ddd14c4554fa5'
+  }.freeze
 
 RSpec.describe Prawn do
   describe 'manual' do
@@ -27,7 +26,7 @@ RSpec.describe Prawn do
 
       hash = Digest::SHA512.hexdigest(s)
 
-      expect(hash).to eq MANUAL_HASH
+      expect(hash).to eq MANUAL_HASH[RUBY_ENGINE]
     end
   end
 end
