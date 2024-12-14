43af228fbdbe2f30ccc993381b824d4fefe315ee
Tomasz Mazur <tomasz.mazur@hey.com>
2024-12-14 10:54:39 +0100

    update MANUAL_HASH

--- spec/prawn_manual_spec.rb
+++ spec/prawn_manual_spec.rb
@@ -6,16 +6,16 @@ require 'digest/sha2'
 MANUAL_HASH =
   case RUBY_ENGINE
   when 'ruby'
-    '06ce69758c64b0e5f14d09474d94ba580aaa4edca7014c6ab5bc9536b5bb0d0c163425aceff74a0ad3867859f8372e07b96e63822cc0e789549bb3a35d3cf185'
+    '46d84d81a99028d9832c1848b6dabea1d0f84af0f25d4b5d473c0a304c3de612aeb54b76b76b870e26b6d547fd6e77b85d82d81a568139ff456959be5e18b057'
   when 'jruby'
-    '31b7c93ddf81f0c734a036644f07541071af36cee1f2e9a6c99847bd98ae6a66a9755afb69f4351fac711382bfc04d1cb50bc00122d7c4d187428f1582680794'
+    'd227479b25382ee61e0cb6345ada735b9ea8105b862c630e08da3c35e2ec702c171d9dd2ab30f71b16ab3ae5144a9cbe0f0591122710562c6624bc01e67a63b9'
   end
 
 RSpec.describe Prawn do
   describe 'manual' do
     # JRuby's zlib is a bit quirky. It sometimes produces different output to
     # libzlib (used by MRI). It's still a proper deflate stream and can be
-    # decompressed just fine but, for whatever reason, compression produces
+    # decompressed just fine, but for whatever reason, compression produces
     # different output.
     #
     # See: https://github.com/jruby/jruby/issues/4244
